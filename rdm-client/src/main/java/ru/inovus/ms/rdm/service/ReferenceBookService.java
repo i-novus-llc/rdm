42f48e31f08fb0e0157862ccb2658b05c670f5d2
Timur Nurdinov <tnurdinov@i-novus.ru>
2018-05-24 10:09:39 +0300

    rename

--- rdm-client/src/main/java/ru/inovus/ms/rdm/service/ReferenceBookService.java
+++ rdm-client/src/main/java/ru/inovus/ms/rdm/service/ReferenceBookService.java
@@ -2,7 +2,7 @@ package ru.inovus.ms.rdm.service;
 
 import java.util.List;
 
-public interface DictionaryService {
+public interface ReferenceBookService {
 
     List<ru.inovus.ms.rdm.model.ReferenceBook> search(ru.inovus.ms.rdm.model.ReferenceBookCriteria referenceBookCriteria);
 
