d05c24e49212072929b4c36421d8541b1d77dde9
Timur Nurdinov <tnurdinov@i-novus.ru>
2018-06-26 16:32:15 +0300

    bf RDM-51 sonar issue fixed

--- rdm-api/src/main/java/ru/inovus/ms/rdm/model/RefBookVersion.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/model/RefBookVersion.java
@@ -2,4 +2,14 @@ package ru.inovus.ms.rdm.model;
 
 
 public class RefBookVersion {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
--- rdm-impl/src/main/java/ru/inovus/ms/rdm/entity/StructureType.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/entity/StructureType.java
@@ -1,6 +1,5 @@
 package ru.inovus.ms.rdm.entity;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
