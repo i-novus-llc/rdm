4ee2dbb09994918c6324cfb6dca6e2e9d3dc17cf
Zagir <znurgaliev@i-novus.ru>
2018-08-07 16:02:49 +0300

    [NF] RDM-62 Генерация файла XLSX

--- rdm-api/src/main/java/ru/inovus/ms/rdm/enumeration/FileType.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/enumeration/FileType.java
@@ -1,8 +1,5 @@
 package ru.inovus.ms.rdm.enumeration;
 
-/**
- * Created by znurgaliev on 06.08.2018.
- */
 public enum FileType {
     ZIP, XLSX
 }
--- rdm-api/src/main/java/ru/inovus/ms/rdm/util/FileNameGenerator.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/util/FileNameGenerator.java
@@ -3,9 +3,6 @@ package ru.inovus.ms.rdm.util;
 import ru.inovus.ms.rdm.enumeration.FileType;
 import ru.inovus.ms.rdm.model.RefBookVersion;
 
-/**
- * Created by znurgaliev on 06.08.2018.
- */
 public interface FileNameGenerator {
 
     String generateName(RefBookVersion version, FileType fileType);
