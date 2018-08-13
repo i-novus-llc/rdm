8184585ce2379b12414655d5f2453c44577cdc47
Zagir <znurgaliev@i-novus.ru>
2018-08-13 11:51:22 +0300

    [NF] RDM-80 Подбор версии для публикации

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/util/SequenceVersionNumberStrategy.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/util/SequenceVersionNumberStrategy.java
@@ -14,29 +14,18 @@ import java.util.regex.Pattern;
 @Component
 public class SequenceVersionNumberStrategy implements VersionNumberStrategy {
 
+    private final Pattern pattern = Pattern.compile("^(\\d*\\.\\d*)");
+
 
     RefBookVersionRepository versionRepository;
 
     @Override
     @Transactional
     public String next(Integer refbookId) {
-        Pattern pattern = Pattern.compile("^(\\d*\\.\\d*)");
         List<RefBookVersionEntity> versionEntityList =
                 versionRepository.findAllByStatusAndRefBookId(RefBookVersionStatus.PUBLISHED, refbookId);
 
-        RefBookVersionEntity maxVersion = versionEntityList.stream().reduce((v1, v2) -> {
-
-            if (v1 == null || v1.getVersion() == null) return v2;
-            if (v2 == null || v2.getVersion() == null) return v1;
-
-            Matcher matcher1 = pattern.matcher(v1.getVersion());
-            Matcher matcher2 = pattern.matcher(v2.getVersion());
-            Double num1 = Double.parseDouble((matcher1.find() ? matcher1.group() : "-1"));
-            Double num2 = Double.parseDouble((matcher2.find() ? matcher2.group() : "-1"));
-            if (num1 > num2) return v1;
-            else if (num2 != -1) return v2;
-            else return null;
-        }).orElse(null);
+        RefBookVersionEntity maxVersion = getMaxVersion(versionEntityList);
 
         if (maxVersion == null) return "1.0";
         Matcher matcher = pattern.matcher(maxVersion.getVersion());
@@ -53,10 +42,26 @@ public class SequenceVersionNumberStrategy implements VersionNumberStrategy {
         return major + "." + minor;
     }
 
+    private RefBookVersionEntity getMaxVersion(List<RefBookVersionEntity> versionEntityList) {
+        return versionEntityList.stream().reduce((v1, v2) -> {
+
+            if (v1 == null || v1.getVersion() == null) return v2;
+            if (v2 == null || v2.getVersion() == null) return v1;
+
+            Matcher matcher1 = pattern.matcher(v1.getVersion());
+            Matcher matcher2 = pattern.matcher(v2.getVersion());
+            Double num1 = Double.parseDouble((matcher1.find() ? matcher1.group() : "-1"));
+            Double num2 = Double.parseDouble((matcher2.find() ? matcher2.group() : "-1"));
+            if (num1 > num2) return v1;
+            else if (num2 != -1) return v2;
+            return null;
+        }).orElse(null);
+    }
+
     @Override
     public boolean check(String version, Integer refbookId) {
         if (version == null || !version.matches("\\d*\\.\\d*")) return false;
-        return !versionRepository.findAllByStatusAndRefBookId(RefBookVersionStatus.PUBLISHED, refbookId)
-                .stream().anyMatch(versionEntity -> versionEntity.getVersion().equals(version));
+        return versionRepository.findAllByStatusAndRefBookId(RefBookVersionStatus.PUBLISHED, refbookId)
+                .stream().noneMatch(versionEntity -> versionEntity.getVersion().equals(version));
     }
 }
