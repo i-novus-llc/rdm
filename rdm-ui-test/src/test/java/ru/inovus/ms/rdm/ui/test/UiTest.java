f9418a12d3d8cbe772867c8c3c489441d9786474
Timur Nurdinov <tnurdinov@i-novus.ru>
2018-08-16 09:34:33 +0300

    nf RDM-98 модуль для тестов

---
+++ rdm-ui-test/src/test/java/ru/inovus/ms/rdm/ui/test/UiTest.java
@@ -0,0 +1,31 @@
+package ru.inovus.ms.rdm.ui.test;
+
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UiTest {
+
+    private static String appUrl = "http://localhost:8081/";
+
+    @BeforeClass
+    public static void init(){
+        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+        String urlFromProperty = System.getProperty("appUrl");
+        if(urlFromProperty != null) {
+            appUrl = urlFromProperty;
+        }
+    }
+
+    @Test
+    public void testOpenStartPage() throws Exception {
+        Selenide.open(appUrl);
+        Selenide.$(Selectors.byText("Код")).shouldHave();
+        Selenide.$(Selectors.byText("Версия")).shouldHave();
+
+    }
+}
