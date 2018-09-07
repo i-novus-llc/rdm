089a12f3057e8644d410f7cd5617f0f56635944d
Zagir <znurgaliev@i-novus.ru>
2018-09-07 17:36:20 +0300

    [NF] RDM-109  Администрирование. Реестр НСИ. Пасспорт ФНСИ изменил список возможных атрибутов

--- rdm-fnsi-impl/src/main/java/ru/inovus/ms/rdm/FnsiPassportSearch.java
+++ rdm-fnsi-impl/src/main/java/ru/inovus/ms/rdm/FnsiPassportSearch.java
@@ -1,12 +1,16 @@
 package ru.inovus.ms.rdm;
 
+import com.google.common.collect.ImmutableMap;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import ru.inovus.ms.rdm.util.PassportPredicateProducer;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static ru.inovus.ms.rdm.repositiory.RefBookVersionPredicates.hasAttributeValue;
@@ -17,13 +21,21 @@ public class FnsiPassportSearch implements PassportPredicateProducer {
 
     @Override
     public Predicate toPredicate(Map<String, String> passportAttributeValueMap) {
+        Map<String, List<String>> orAttributes = ImmutableMap.of(
+                "name", Arrays.asList("fullName", "shortName"),
+                "OID", Arrays.asList("OID.name", "OID2.name")
+        );
         Map<String, String> tempMap = new HashMap<>(passportAttributeValueMap);
-        String name = tempMap.remove("name");
-        String oid = tempMap.remove("OID");
+
         BooleanBuilder where = new BooleanBuilder();
-        tempMap.forEach((k, v) -> where.and(hasAttributeValue(k, v)));
-        if (name != null) where.and(hasAttributeValue("fullName", name).or(hasAttributeValue("shortName", name)));
-        if (oid != null) where.and(hasAttributeValue("OID", oid).or(hasAttributeValue("OID2", oid)));
+        tempMap.forEach((k, v) -> {
+            if (orAttributes.containsKey(k)){
+                where.and(orAttributes.get(k).stream()
+                        .map(orAttr -> hasAttributeValue(orAttr, v))
+                        .reduce(BooleanExpression::or).get());
+            } else
+            where.and(hasAttributeValue(k, v));
+        });
         return where.getValue();
     }
 
