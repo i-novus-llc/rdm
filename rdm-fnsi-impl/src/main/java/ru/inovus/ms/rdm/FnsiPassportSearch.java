81722ec201893d0e473c87af334ffaeb968af7be
Zagir <znurgaliev@i-novus.ru>
2018-09-07 18:06:42 +0300

    [NF] RDM-109  Администрирование. Реестр НСИ. Пасспорт ФНСИ изменил список возможных атрибутов

--- rdm-fnsi-impl/src/main/java/ru/inovus/ms/rdm/FnsiPassportSearch.java
+++ rdm-fnsi-impl/src/main/java/ru/inovus/ms/rdm/FnsiPassportSearch.java
@@ -4,12 +4,12 @@ import com.google.common.collect.ImmutableMap;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import ru.inovus.ms.rdm.util.PassportPredicateProducer;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,14 +21,14 @@ public class FnsiPassportSearch implements PassportPredicateProducer {
 
     @Override
     public Predicate toPredicate(Map<String, String> passportAttributeValueMap) {
+        if (passportAttributeValueMap == null) return Expressions.TRUE;
         Map<String, List<String>> orAttributes = ImmutableMap.of(
                 "name", Arrays.asList("fullName", "shortName"),
                 "OID", Arrays.asList("OID.name", "OID2.name")
         );
-        Map<String, String> tempMap = new HashMap<>(passportAttributeValueMap);
 
         BooleanBuilder where = new BooleanBuilder();
-        tempMap.forEach((k, v) -> {
+        passportAttributeValueMap.forEach((k, v) -> {
             if (orAttributes.containsKey(k)){
                 where.and(orAttributes.get(k).stream()
                         .map(orAttr -> hasAttributeValue(orAttr, v))
