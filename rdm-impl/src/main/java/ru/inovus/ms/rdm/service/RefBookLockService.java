3dbac9b38867a6e3fa552d7936881acce02cff1c
Zagir <znurgaliev@i-novus.ru>
2018-09-19 10:34:19 +0300

    [NF] RDM-95  Блокировать действия над черновиком при загрузке и публикации данных

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/service/RefBookLockService.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/service/RefBookLockService.java
@@ -2,13 +2,11 @@ package ru.inovus.ms.rdm.service;
 
 import ru.inovus.ms.rdm.entity.RefBookEntity;
 
-import javax.annotation.PostConstruct;
-
 /**
  * Created by znurgaliev on 19.09.2018.
  */
 public interface RefBookLockService {
-    @PostConstruct
+
     void cleanOperations();
 
     void setRefBookPublishing(Integer refBookId);
