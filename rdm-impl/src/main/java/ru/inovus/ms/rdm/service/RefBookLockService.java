a93be16406151a1fe429d9414aac737995e1a09c
Zagir <znurgaliev@i-novus.ru>
2018-09-19 10:33:59 +0300

    [NF] RDM-95  Блокировать действия над черновиком при загрузке и публикации данных

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/service/RefBookLockService.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/service/RefBookLockService.java
@@ -1,95 +1,23 @@
 package ru.inovus.ms.rdm.service;
 
-import net.n2oapp.platform.i18n.UserException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import ru.inovus.ms.rdm.entity.RefBookEntity;
-import ru.inovus.ms.rdm.entity.RefBookOperationEntity;
-import ru.inovus.ms.rdm.entity.RefBookVersionEntity;
-import ru.inovus.ms.rdm.enumeration.RefBookOperation;
-import ru.inovus.ms.rdm.repositiory.RefBookOperationRepository;
-import ru.inovus.ms.rdm.repositiory.RefBookRepository;
-import ru.inovus.ms.rdm.repositiory.RefBookVersionRepository;
 
 import javax.annotation.PostConstruct;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
-@Service
-public class RefBookLockService {
-
-    private static final Logger logger = LoggerFactory.getLogger(RefBookLockService.class);
-
-    private static final String DEFAULT_USER = "admin";
-    private String instanceId = "localhost";
-
-    private RefBookRepository refBookRepository;
-    private RefBookOperationRepository operationRepository;
-    private RefBookVersionRepository versionRepository;
-
-    @Autowired
-    public RefBookLockService(RefBookRepository refBookRepository, RefBookOperationRepository operationRepository,
-                              RefBookVersionRepository versionRepository) {
-        this.refBookRepository = refBookRepository;
-        this.operationRepository = operationRepository;
-        this.versionRepository = versionRepository;
-        try {
-            this.instanceId = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            logger.error("cannot.read.host.address", e);
-        }
-
-    }
-
+/**
+ * Created by znurgaliev on 19.09.2018.
+ */
+public interface RefBookLockService {
     @PostConstruct
-    public void cleanOperations() {
-        operationRepository.deleteAllByInstanceId(instanceId);
-    }
+    void cleanOperations();
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void setRefBookPublishing(Integer refBookId) {
-        RefBookEntity refBook = refBookRepository.findOne(refBookId);
-        validateRefBookNotBusy(refBook);
-        operationRepository.save(new RefBookOperationEntity(refBook, RefBookOperation.PUBLISHING, instanceId, DEFAULT_USER));
+    void setRefBookPublishing(Integer refBookId);
 
-    }
+    void setRefBookUploading(Integer refBookId);
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void setRefBookUploading(Integer refBookId) {
-        RefBookEntity refBook = refBookRepository.findOne(refBookId);
-        validateRefBookNotBusy(refBook);
-        operationRepository.save(new RefBookOperationEntity(refBook, RefBookOperation.UPLOADING, instanceId, DEFAULT_USER));
+    void deleteRefBookAction(Integer refBookId);
 
-    }
+    void validateRefBookNotBusyByVersionId(Integer versionId);
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void deleteRefBookAction(Integer refBookId) {
-        RefBookEntity refBook = refBookRepository.findOne(refBookId);
-        if (refBook != null && refBook.getCurrentOperation() != null) {
-            operationRepository.delete(refBook.getCurrentOperation().getId());
-            refBook.setCurrentOperation(null);
-        }
-
-    }
-
-    public void validateRefBookNotBusyByVersionId(Integer versionId) {
-        RefBookVersionEntity versionEntity = versionRepository.findOne(versionId);
-        if (versionEntity != null) {
-            validateRefBookNotBusy(versionEntity.getRefBook());
-        }
-    }
-
-    public void validateRefBookNotBusy(RefBookEntity refBookEntity) {
-        if (refBookEntity.getCurrentOperation() != null) {
-            if (RefBookOperation.PUBLISHING.equals(refBookEntity.getCurrentOperation().getOperation())) {
-                throw new UserException("draft.is.publishing");
-            } else if (RefBookOperation.UPLOADING.equals(refBookEntity.getCurrentOperation().getOperation())) {
-                throw new UserException("draft.is.uploading");
-            }
-        }
-    }
+    void validateRefBookNotBusy(RefBookEntity refBookEntity);
 }
