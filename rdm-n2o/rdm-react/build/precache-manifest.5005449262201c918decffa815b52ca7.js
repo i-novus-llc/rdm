850c1a0f1540a061ff3fa274cadc6d3a7e8bf9fc
zagirnur <zagirnur@gmail.com>
2018-12-06 11:53:37 +0300

    [CF] RDM-180 Обновил UI N2O до версии 7.0.0-RC.5

---
+++ rdm-n2o/rdm-react/build/precache-manifest.5005449262201c918decffa815b52ca7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f7f2b55e1901d853ffff",
+    "url": "/static/css/main.8632125b.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3f815ad073ed50569363",
+    "url": "/static/js/1.3f815ad0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7f2b55e1901d853ffff",
+    "url": "/static/js/main.f7f2b55e.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3f815ad073ed50569363",
+    "url": "/static/css/1.05ced9f1.chunk.css"
+  },
+  {
+    "revision": "975abdad98c41a1969a43b70183b04e3",
+    "url": "/index.html"
+  }
+];
