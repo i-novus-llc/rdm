426ef7d11dcd6922d83526bab0b6f2d1292e9a2e
Zagir <znurgaliev@i-novus.ru>
2018-12-13 12:41:27 +0300

    [CF] RDM-255 Обновление версии N2O до RC10

---
+++ rdm-n2o/rdm-react/build/precache-manifest.3b0deb67331b763453b504056bc971ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "407a7b12c197c863415a",
+    "url": "/static/css/main.5fbad8d2.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3ca738dd0abbaf5a09b3",
+    "url": "/static/js/1.3ca738dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "407a7b12c197c863415a",
+    "url": "/static/js/main.407a7b12.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3ca738dd0abbaf5a09b3",
+    "url": "/static/css/1.339dbed0.chunk.css"
+  },
+  {
+    "revision": "4157b8b04ddd3cccf04666138eda6d4b",
+    "url": "/index.html"
+  }
+];
--- rdm-n2o/rdm-react/build/service-worker.js
+++ rdm-n2o/rdm-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbe8db3dd17752dad3e31ce803edb511.js"
+  "/precache-manifest.3b0deb67331b763453b504056bc971ff.js"
 );
 
 workbox.clientsClaim();
