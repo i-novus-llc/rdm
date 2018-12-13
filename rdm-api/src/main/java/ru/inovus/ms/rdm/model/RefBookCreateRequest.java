25d66519f1f1a893a72a4d835110a7d521c34c36
Zagir <znurgaliev@i-novus.ru>
2018-12-13 13:18:09 +0300

    [NF] RDM-160 Форма паспорта, просмотр

--- rdm-api/src/main/java/ru/inovus/ms/rdm/model/RefBookCreateRequest.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/model/RefBookCreateRequest.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class RefBookCreateRequest {
 
     private String code;
+    private String category;
     private Map<String, String> passport;
 
     public RefBookCreateRequest() {
@@ -15,12 +16,26 @@ public class RefBookCreateRequest {
         this.passport = passport;
     }
 
+    public RefBookCreateRequest(String code, Map<String, String> passport, String category) {
+        this.code = code;
+        this.passport = passport;
+        this.category = category;
+    }
+
     public String getCode() {
         return code;
     }
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public Map<String, String> getPassport() {
