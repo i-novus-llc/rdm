93de98ac9974e08697d7230b73a2e672cf793f53
Timur Nurdinov <tnurdinov@i-novus.ru>
2018-12-21 19:33:25 +0300

    bf RDM-176 пропали кнопки и не было кнопки выход

--- rdm-n2o/rdm-react/src/App.js
+++ rdm-n2o/rdm-react/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import N2O from 'n2o';
 import DataGrid from 'n2o-data-grid';
 import 'n2o-data-grid/css/DataGrid.css';
+import { authProvider } from 'n2o-auth';
+
 
 
 const config = {
@@ -14,7 +16,12 @@ const config = {
         filterable={true}
         {...props}
         />
-  }
+  },
+security: {
+    authProvider,
+    externalLoginUrl: '/'
+}
+
 };
 
 class App extends Component {
