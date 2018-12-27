06dff97b68373501fa61e62bacbc5e9f78214e3b
Aygul Mardanova <amardanova@i-novus.ru>
2018-12-27 17:18:24 +0300

    nf RDM-180 аудит входа/выхода

--- rdm-n2o/src/main/java/ru/inovus/ms/rdm/SecurityConfig.java
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/SecurityConfig.java
@@ -1,20 +1,52 @@
 package ru.inovus.ms.rdm;
 
 import net.n2oapp.framework.security.auth.oauth2.OpenIdSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import ru.inovus.ms.rdm.handler.AuditAuthenticationSuccessHandler;
+import ru.inovus.ms.rdm.handler.AuditLogoutHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends OpenIdSecurityConfigurerAdapter {
 
+    @Autowired
+    AuditAuthenticationSuccessHandler auditAuthenticationSuccessHandler;
+
+    @Autowired
+    AuditLogoutHandler auditLogoutHandler;
+
     @Override
     protected void authorize(ExpressionUrlAuthorizationConfigurer<HttpSecurity>
                                      .ExpressionInterceptUrlRegistry url) throws Exception {
-        url.anyRequest().authenticated();//все запросы авторизованы
+        //все запросы авторизованы
+        url.anyRequest().authenticated()
+                .and()
+                .logout().addLogoutHandler(auditLogoutHandler)
+                .and()
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
     }
 
+    private OAuth2ClientAuthenticationProcessingFilter ssoFilter() {
+        OAuth2SsoProperties ssoProps = this.getApplicationContext().getBean(OAuth2SsoProperties.class);
+
+        OAuth2ClientAuthenticationProcessingFilter ssoFilter =
+                new OAuth2ClientAuthenticationProcessingFilter(ssoProps.getLoginPath());
+        ssoFilter.setAuthenticationSuccessHandler(auditAuthenticationSuccessHandler);
+        ssoFilter.setRestTemplate(this.getApplicationContext()
+                .getBean(UserInfoRestTemplateFactory.class).getUserInfoRestTemplate());
+        ssoFilter.setTokenServices(this.getApplicationContext()
+                .getBean(ResourceServerTokenServices.class));
+        ssoFilter.setApplicationEventPublisher(this.getApplicationContext());
+        return ssoFilter;
+    }
 
 }
---
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/handler/AuditAuthenticationSuccessHandler.java
@@ -0,0 +1,33 @@
+package ru.inovus.ms.rdm.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import ru.inovus.ms.rdm.model.audit.AuditAction;
+import ru.inovus.ms.rdm.model.audit.AuditLog;
+import ru.inovus.ms.rdm.service.api.AuditLogService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class AuditAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Autowired
+    AuditLogService auditLogService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws ServletException, IOException {
+        AuditLog auditLog =
+                new AuditLog(null, authentication.getName(), LocalDateTime.now(), AuditAction.LOGIN, null);
+        auditLogService.addAction(auditLog);
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+}
