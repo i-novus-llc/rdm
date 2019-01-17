9a535479674df4918304dfde8929ad1583bce975
Timur Nurdinov <tnurdinov@i-novus.ru>
2019-01-17 10:34:49 +0300

    cf  RDM-324 если нет структуры то грузит из файла

--- rdm-n2o/src/main/java/ru/inovus/ms/rdm/audit/creator/UploadAuditLogCreator.java
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/audit/creator/UploadAuditLogCreator.java
@@ -4,9 +4,9 @@ import net.n2oapp.criteria.dataset.DataSet;
 import net.n2oapp.framework.api.ui.ActionRequestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.inovus.ms.rdm.model.RefBookVersion;
 import ru.inovus.ms.rdm.model.audit.AuditAction;
 import ru.inovus.ms.rdm.model.audit.AuditLog;
+import ru.inovus.ms.rdm.service.api.RefBookService;
 import ru.inovus.ms.rdm.service.api.VersionService;
 
 import java.time.LocalDateTime;
@@ -19,15 +19,25 @@ public class UploadAuditLogCreator implements AuditLogCreator {
     @Autowired
     VersionService versionService;
 
+    @Autowired
+    private RefBookService refBookService;
+
     @Override
     public AuditLog create(ActionRequestInfo requestInfo, DataSet dataSet) {
 
-        RefBookVersion refBookVersion = versionService.getById(dataSet.getInteger("versionId"));
+        final Integer refBookId = dataSet.getInteger("refBookId");
+        String refBookCode = null;
+        if (refBookId != null) {
+            refBookCode = refBookService.getCode(refBookId);
+        } else {
+            refBookCode = versionService.getById(dataSet.getInteger("versionId")).getCode();
+        }
+
 
         return new AuditLog(null,
                 requestInfo.getUser().getUsername(),
                 LocalDateTime.now(),
                 auditAction,
-                refBookVersion.getCode());
+                refBookCode);
     }
 }
