963d320312e3889ac10b1277590e766aa157c955
Aygul Mardanova <amardanova@i-novus.ru>
2019-02-13 15:23:29 +0300

    [CF] RDM-309 Обновление n2o до версии 7.0.5

---
+++ rdm-n2o/rdm-react/build/precache-manifest.fbb43587556b56da23d3aaa824a35429.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "a5fcdc23d38228dd767c",
+    "url": "./static/css/main.7a17e245.chunk.css"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9a9d0cf5c8ef668f987c",
+    "url": "./static/js/1.9a9d0cf5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a5fcdc23d38228dd767c",
+    "url": "./static/js/main.a5fcdc23.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9a9d0cf5c8ef668f987c",
+    "url": "./static/css/1.8ca52469.chunk.css"
+  },
+  {
+    "revision": "411cc7be70b625233e37598c1db667e6",
+    "url": "./index.html"
+  }
+];
