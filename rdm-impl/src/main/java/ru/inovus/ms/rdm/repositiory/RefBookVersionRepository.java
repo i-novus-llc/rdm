7bc2b2f4c3f6287eae1bc422015db394668ff42e
arahmatullin <arahmatullin@i-novus.ru>
2019-05-16 12:40:43 +0300

    cf RDM-452 replace findStatusVersions with findAll.

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/repositiory/RefBookVersionRepository.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/repositiory/RefBookVersionRepository.java
@@ -25,19 +25,17 @@ public interface RefBookVersionRepository extends
     @Query("select v from RefBookVersionEntity v where v.refBook.code = ?1 and v.fromDate <= ?2 and (v.toDate > ?2 or v.toDate is null)")
     RefBookVersionEntity findActualOnDate(String refBookCode, LocalDateTime date);
 
-    @Query("select v from RefBookVersionEntity v where v.refBook.id = ?1 and v.status = ?2 order by v.fromDate desc")
-    List<RefBookVersionEntity> findStatusVersions(Integer refBookId, RefBookVersionStatus status);
+    List<RefBookVersionEntity> findAllByRefBookIdAndStatusOrderByFromDateDesc(Integer refBookId, RefBookVersionStatus status);
 
-    @Query("select v from RefBookVersionEntity v where v.refBook.code = ?1 and v.status = ?2 order by v.fromDate desc")
-    List<RefBookVersionEntity> findStatusVersions(String refBookCode, RefBookVersionStatus status);
+    List<RefBookVersionEntity> findAllByRefBookCodeAndStatusOrderByFromDateDesc(String refBookCode, RefBookVersionStatus status);
 
     default RefBookVersionEntity findLastVersion(Integer refBookId, RefBookVersionStatus status) {
-        List<RefBookVersionEntity> versions = findStatusVersions(refBookId, status);
+        List<RefBookVersionEntity> versions = findAllByRefBookIdAndStatusOrderByFromDateDesc(refBookId, status);
         return (!CollectionUtils.isEmpty(versions)) ? versions.get(0) : null;
     }
 
     default RefBookVersionEntity findLastVersion(String refBookCode, RefBookVersionStatus status) {
-        List<RefBookVersionEntity> versions = findStatusVersions(refBookCode, status);
+        List<RefBookVersionEntity> versions = findAllByRefBookCodeAndStatusOrderByFromDateDesc(refBookCode, status);
         return (!CollectionUtils.isEmpty(versions)) ? versions.get(0) : null;
     }
 
