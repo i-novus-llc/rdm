e0b74b2baa9f3c62ec416e62657dfd164a6b0bed
arahmatullin <arahmatullin@i-novus.ru>
2019-05-21 07:49:42 +0300

    cf RDM-420 fix test buf after master merge.

--- rdm-n2o/src/test/java/ru/inovus/ms/rdm/service/StructureControllerTest.java
+++ rdm-n2o/src/test/java/ru/inovus/ms/rdm/service/StructureControllerTest.java
@@ -197,7 +197,6 @@ public class StructureControllerTest extends TestCase {
         RefBookVersion referenceVersion = new RefBookVersion();
         referenceVersion.setId(referenceVersionId);
         referenceVersion.setStructure(new Structure(singletonList(build(referenceAttribute, null, FieldType.INTEGER, null)), null));
-        when(versionService.getLastPublishedVersion(eq(referenceCode))).thenReturn(referenceVersion);
 
         RestPage<ReadAttribute> page = structureController.getPage(new AttributeCriteria(null, versionId));
         ReadAttribute actual = page.getContent().get(0);
