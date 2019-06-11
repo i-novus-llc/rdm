ec03792b667e9dc5912816cf06d02ede56e9f870
arahmatullin <arahmatullin@i-novus.ru>
2019-06-11 11:20:51 +0300

    cf RDM-422 restyle some code (RDM-469).

--- rdm-n2o/src/main/java/ru/inovus/ms/rdm/service/CreateDraftController.java
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/service/CreateDraftController.java
@@ -123,16 +123,18 @@ public class CreateDraftController {
 
     private Long calculateNewSystemId(Long oldSystemId, Integer oldVersionId, Integer newVersionId) {
         if (oldSystemId == null) return null;
+
         SearchDataCriteria criteria = new SearchDataCriteria();
         AttributeFilter recordIdFilter = new AttributeFilter("SYS_RECORDID", oldSystemId.intValue(), FieldType.INTEGER);
         criteria.setAttributeFilter(singleton(singletonList(recordIdFilter)));
+
         Page<RefBookRowValue> oldRow = versionService.search(oldVersionId, criteria);
         if (CollectionUtils.isEmpty(oldRow.getContent())) throw new NotFoundException("record not found");
-
         String hash = oldRow.getContent().get(0).getHash();
 
         AttributeFilter hashFilter = new AttributeFilter("SYS_HASH", hash, FieldType.STRING);
         final SearchDataCriteria hashCriteria = new SearchDataCriteria(ImmutableSet.of(singletonList(hashFilter)), null);
+
         final Page<RefBookRowValue> newRow = versionService.search(newVersionId, hashCriteria);
         if (CollectionUtils.isEmpty(newRow.getContent())) throw new NotFoundException("record not found");
         return newRow.getContent().get(0).getSystemId();
