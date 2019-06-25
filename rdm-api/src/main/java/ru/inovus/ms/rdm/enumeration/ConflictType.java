91e5885a8985b40d5f02bca740cfad51b518335d
arahmatullin <arahmatullin@i-novus.ru>
2019-06-25 12:44:39 +0300

    cf RDM-422 add description comment for enum values (RDM-491).

--- rdm-api/src/main/java/ru/inovus/ms/rdm/enumeration/ConflictType.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/enumeration/ConflictType.java
@@ -1,6 +1,11 @@
 package ru.inovus.ms.rdm.enumeration;
 
+/**
+ * Тип конфликта.
+ */
 public enum ConflictType {
 
-    UPDATED, DELETED
+    // Изменения в данных:
+    UPDATED,            // Запись обновлена
+    DELETED             // Запись удалена
 }
