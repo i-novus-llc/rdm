72e8ec8af74af13ad6d9e251665c2796b5b5ac35
arahmatullin <arahmatullin@i-novus.ru>
2019-06-28 11:17:18 +0300

    bf RDM-422 exclude unused annotations (RDM-491).

--- rdm-n2o/src/main/java/ru/inovus/ms/rdm/model/UiRefBookPublish.java
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/model/UiRefBookPublish.java
@@ -1,16 +1,9 @@
 package ru.inovus.ms.rdm.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 import java.util.Map;
 
-@ApiModel("Данные для публикации справочника")
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UiRefBookPublish extends RefBook {
 
-    @ApiModelProperty("Конфликтующие справочники")
     private Map<String, String> conflictReferrerNames;
 
     public UiRefBookPublish(RefBook refBook) {
