c1fba9771a4dd2ca3b2d44789b775442ef9b6df3
arahmatullin <arahmatullin@i-novus.ru>
2019-07-12 07:53:42 +0300

    cf RDM-422 add `testFilteredIteration` (RDM-495).

--- rdm-impl/src/test/java/ru/inovus/ms/rdm/PageIteratorTest.java
+++ rdm-impl/src/test/java/ru/inovus/ms/rdm/PageIteratorTest.java
@@ -19,18 +19,19 @@ public class PageIteratorTest {
 
     private static final List<String> allContent = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8");
 
-
     @Test
     public void testIteration() {
+
         Function<TestCriteria, Page<String>> pageSource = (criteria) -> {
             int total = allContent.size();
-            List<String> content = allContent.subList(Math.min(criteria.getPageNumber()*criteria.getPageSize(),allContent.size()), Math.min((criteria.getPageNumber() + 1)*criteria.getPageSize(), allContent.size()));
-            return new PageImpl<String>(content, criteria, total);
+            int offset = criteria.getPageNumber() * criteria.getPageSize();
+            List<String> content = allContent.subList(Math.min(offset, total), Math.min(offset + criteria.getPageSize(), total));
+            return new PageImpl<>(content, criteria, total);
         };
 
         TestCriteria criteria = new TestCriteria();
         criteria.setPageSize(3);
-        criteria.setOrders(Arrays.asList(new Sort.Order(Sort.Direction.ASC, "id")));
+        criteria.setOrders(Collections.singletonList(new Sort.Order(Sort.Direction.ASC, "id")));
         PageIterator<String, TestCriteria> pageIterator = new PageIterator<>(pageSource, criteria);
         List<List<String>> expectedPages = new ArrayList<>();
         expectedPages.add(Arrays.asList("1", "2", "3"));
@@ -40,8 +41,36 @@ public class PageIteratorTest {
         for (int i = 0; i<3; i++) {
             Assert.assertTrue((i+1) + " - page number of 3 not found", pageIterator.hasNext());
             Page<String> page = pageIterator.next();
-            String displayContent = page.getContent().stream().collect(Collectors.joining(","));
-            Assert.assertTrue(displayContent+ " - unexpected content", expectedPages.remove(page.getContent()));
+            String displayContent = String.join(",", page.getContent());
+            Assert.assertTrue(displayContent + " - unexpected content", expectedPages.remove(page.getContent()));
+        }
+    }
+
+    //@Test
+    public void testFilteredIteration() {
+
+        Function<TestCriteria, Page<String>> pageSource = (criteria) -> {
+            int total = allContent.size();
+            int offset = criteria.getPageNumber() * criteria.getPageSize();
+            List<String> content = allContent.subList(Math.min(offset, total), Math.min(offset + criteria.getPageSize(), total));
+            List<String> subContent = content.stream().filter(value -> Integer.parseInt(value) % 4 == 0).collect(Collectors.toUnmodifiableList());
+            return new PageImpl<>(subContent, criteria, subContent.size());
+        };
+
+        TestCriteria criteria = new TestCriteria();
+        criteria.setPageSize(3);
+        criteria.setOrders(Collections.singletonList(new Sort.Order(Sort.Direction.ASC, "id")));
+        PageIterator<String, TestCriteria> pageIterator = new PageIterator<>(pageSource, criteria);
+        List<List<String>> expectedPages = new ArrayList<>();
+        expectedPages.add(Collections.emptyList());
+        expectedPages.add(Collections.singletonList("4"));
+        expectedPages.add(Collections.singletonList("8"));
+
+        for (int i = 0; i < 3; i++) {
+            Assert.assertTrue((i+1) + " - page number of 3 not found", pageIterator.hasNext());
+            Page<String> page = pageIterator.next();
+            String displayContent = String.join(",", page.getContent());
+            Assert.assertTrue(displayContent + " - unexpected content", expectedPages.remove(page.getContent()));
         }
     }
 
