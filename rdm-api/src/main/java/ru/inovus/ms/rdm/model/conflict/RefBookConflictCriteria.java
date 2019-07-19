859f27642384775ed1ba25a5d150f57c224f77d3
arahmatullin <arahmatullin@i-novus.ru>
2019-07-19 07:53:48 +0300

    bf RDM-422 fix bug with `publishedVersionRefBookId` (RDM-490).

--- rdm-api/src/main/java/ru/inovus/ms/rdm/model/conflict/RefBookConflictCriteria.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/model/conflict/RefBookConflictCriteria.java
@@ -25,7 +25,7 @@ public class RefBookConflictCriteria extends AbstractCriteria {
     private Integer publishedVersionId;
 
     @ApiModelProperty("Идентификатор опубликованного справочника")
-    @QueryParam("publishedVersionId")
+    @QueryParam("publishedVersionRefBookId")
     private Integer publishedVersionRefBookId;
 
     @ApiModelProperty("Системный идентификатор записи с конфликтом")
