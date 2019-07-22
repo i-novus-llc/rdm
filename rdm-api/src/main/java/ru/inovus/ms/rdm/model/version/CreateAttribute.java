7e9ce3951dce93b2555eeb992e683f62b4ca8c71
arahmatullin <arahmatullin@i-novus.ru>
2019-07-22 10:38:44 +0300

    сf RDM-422 extract `RefBookVersionAttribute` (RDM-490).

--- rdm-api/src/main/java/ru/inovus/ms/rdm/model/version/CreateAttribute.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/model/version/CreateAttribute.java
@@ -2,43 +2,12 @@ package ru.inovus.ms.rdm.model.version;
 
 import ru.inovus.ms.rdm.model.Structure;
 
-public class CreateAttribute {
+public class CreateAttribute extends RefBookVersionAttribute {
 
-    private Integer versionId;
-
-    private Structure.Attribute attribute;
-
-    private Structure.Reference reference;
-
+    @SuppressWarnings("unused")
     public CreateAttribute() {}
 
     public CreateAttribute(Integer versionId, Structure.Attribute attribute, Structure.Reference reference) {
-        this.versionId = versionId;
-        this.attribute = attribute;
-        this.reference = reference;
-    }
-
-    public Integer getVersionId() {
-        return versionId;
-    }
-
-    public void setVersionId(Integer versionId) {
-        this.versionId = versionId;
-    }
-
-    public Structure.Attribute getAttribute() {
-        return attribute;
-    }
-
-    public void setAttribute(Structure.Attribute attribute) {
-        this.attribute = attribute;
-    }
-
-    public Structure.Reference getReference() {
-        return reference;
-    }
-
-    public void setReference(Structure.Reference reference) {
-        this.reference = reference;
+        super(versionId, attribute, reference);
     }
 }
