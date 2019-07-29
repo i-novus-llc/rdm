ade3f3dcfc89baacaf66bb1643396566e13de907
arahmatullin <arahmatullin@i-novus.ru>
2019-07-29 12:30:47 +0300

    cf RDM-539 separate conflict types.

--- rdm-api/src/main/java/ru/inovus/ms/rdm/util/ConflictUtils.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/util/ConflictUtils.java
@@ -3,9 +3,30 @@ package ru.inovus.ms.rdm.util;
 import ru.i_novus.platform.datastorage.temporal.enums.DiffStatusEnum;
 import ru.inovus.ms.rdm.enumeration.ConflictType;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
 public class ConflictUtils {
 
+    private static final List<ConflictType> DATA_CONFLICT_TYPES = asList(
+            ConflictType.UPDATED, ConflictType.DELETED, ConflictType.ALTERED
+    );
+
+    private static final List<ConflictType> STRUCTURE_CONFLICT_TYPES = singletonList(
+            ConflictType.DISPLAY_DAMAGED
+    );
+
     private ConflictUtils() {
+    }
+
+    public static List<ConflictType> getDataConflictTypes() {
+        return DATA_CONFLICT_TYPES;
+    }
+
+    public static List<ConflictType> getStructureConflictTypes() {
+        return STRUCTURE_CONFLICT_TYPES;
     }
 
     public static ConflictType diffStatusToConflictType(DiffStatusEnum diffStatus) {
