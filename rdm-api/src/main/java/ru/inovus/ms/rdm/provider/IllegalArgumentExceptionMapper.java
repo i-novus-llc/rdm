a71a6ef34144c37b3fd176f64209e167faf3ed5f
arahmatullin <arahmatullin@i-novus.ru>
2019-07-31 11:40:18 +0300

    bf RDM-539 fix SonarLint issues.

--- rdm-api/src/main/java/ru/inovus/ms/rdm/provider/IllegalArgumentExceptionMapper.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/provider/IllegalArgumentExceptionMapper.java
@@ -12,15 +12,15 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class IllegalArgumentExceptionMapper implements RestExceptionMapper<IllegalArgumentException> {
 
+    private static final String ARGUMENT_ERROR =  "arg.error";
+
+    private static final Logger logger = LoggerFactory.getLogger(IllegalArgumentExceptionMapper.class);
+
     private Messages messages;
 
     public IllegalArgumentExceptionMapper(Messages messages) {
         this.messages = messages;
     }
-
-    private static final String ARGUMENT_ERROR =  "arg.error";
-
-    private static final Logger logger = LoggerFactory.getLogger(IllegalArgumentExceptionMapper.class);
 
     @Override
     public RestMessage toMessage(IllegalArgumentException e) {
