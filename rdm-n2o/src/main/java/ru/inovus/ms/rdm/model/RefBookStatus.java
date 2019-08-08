cbe3e28fb0375bcbe6c63f65cf1326f52df775d8
arahmatullin <arahmatullin@i-novus.ru>
2019-08-08 08:29:44 +0300

    nf EPMP-36 filter status list according to object-filters (EPMP-38).

--- rdm-n2o/src/main/java/ru/inovus/ms/rdm/model/RefBookStatus.java
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/model/RefBookStatus.java
@@ -4,5 +4,10 @@ package ru.inovus.ms.rdm.model;
  * Created by znurgaliev on 07.11.2018.
  */
 public enum RefBookStatus {
-    ARCHIVED, HAS_DRAFT, PUBLISHED
+    // Состояние справочника:
+    ARCHIVED,       // Архивный
+
+    // Состояние версии справочника:
+    HAS_DRAFT,      // В работе (есть черновик)
+    PUBLISHED       // Опубликован
 }
--- rdm-n2o/src/main/java/ru/inovus/ms/rdm/service/RefBookController.java
+++ rdm-n2o/src/main/java/ru/inovus/ms/rdm/service/RefBookController.java
@@ -2,25 +2,43 @@ package ru.inovus.ms.rdm.service;
 
 import net.n2oapp.framework.access.simple.PermissionApi;
 import net.n2oapp.framework.api.user.StaticUserContext;
+import net.n2oapp.platform.i18n.Messages;
+import net.n2oapp.platform.jaxrs.RestPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import ru.inovus.ms.rdm.criteria.RefBookStatusCriteria;
+import ru.inovus.ms.rdm.model.RefBookStatus;
+import ru.inovus.ms.rdm.model.UiRefBookStatus;
 import ru.inovus.ms.rdm.model.refbook.RefBook;
 import ru.inovus.ms.rdm.model.refbook.RefBookCriteria;
 import ru.inovus.ms.rdm.service.api.RefBookService;
 import ru.inovus.ms.rdm.util.RdmPermission;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Controller
 public class RefBookController {
 
+    private static final String REF_BOOK_STATUS_ARCHIVED = "refbook.status.archived";
+    private static final String REF_BOOK_STATUS_HAS_DRAFT = "refbook.status.has_draft";
+    private static final String REF_BOOK_STATUS_PUBLISHED = "refbook.status.published";
+
+    private Messages messages;
+
     private RefBookService refBookService;
 
     private RdmPermission rdmPermission;
 
     @Autowired
-    public RefBookController(RefBookService refBookService, PermissionApi permissionApi) {
+    public RefBookController(Messages messages,
+                             RefBookService refBookService,
+                             PermissionApi permissionApi) {
+        this.messages = messages;
         this.refBookService = refBookService;
 
         this.rdmPermission = new RdmPermission(StaticUserContext.getUserContext(), permissionApi);
@@ -29,7 +47,7 @@ public class RefBookController {
     /**
      * Поиск последней версии справочника для открытия на просмотр/редактирование.
      */
-    @SuppressWarnings("unused")
+    @SuppressWarnings("unused") // used in: refBookVersion.query.xml
     public RefBook searchLastVersion(RefBookCriteria criteria) {
 
         Page<RefBook> refBooks = refBookService.searchVersions(permitCriteria(criteria));
@@ -47,5 +65,23 @@ public class RefBookController {
         criteria.setExcludeDraft(rdmPermission.excludeDraft());
 
         return criteria;
+    }
+
+    @SuppressWarnings("unused") // used in: refBookStatusList.query.xml
+    public Page<UiRefBookStatus> getStatusList(RefBookStatusCriteria criteria) {
+
+        List<UiRefBookStatus> list = new ArrayList<>();
+        list.add(getRefBookStatus(RefBookStatus.PUBLISHED, REF_BOOK_STATUS_PUBLISHED));
+
+        if (!criteria.getExcludeDraft())
+            list.add(getRefBookStatus(RefBookStatus.HAS_DRAFT, REF_BOOK_STATUS_HAS_DRAFT));
+        if (!criteria.getNonArchived())
+            list.add(getRefBookStatus(RefBookStatus.ARCHIVED, REF_BOOK_STATUS_ARCHIVED));
+
+        return new RestPage<>(list, Pageable.unpaged(), list.size());
+    }
+
+    private UiRefBookStatus getRefBookStatus(RefBookStatus status, String code) {
+        return new UiRefBookStatus(status, messages.getMessage(code));
     }
 }
