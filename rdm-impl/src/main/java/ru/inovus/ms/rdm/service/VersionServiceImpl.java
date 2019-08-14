418f3f0baf50b5b4014a6d144e9ea326bd1a7a3d
arahmatullin <arahmatullin@i-novus.ru>
2019-08-14 07:27:56 +0300

    bf RDM-543 remove message constant duplicate

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/service/VersionServiceImpl.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/service/VersionServiceImpl.java
@@ -5,7 +5,6 @@ import net.n2oapp.platform.i18n.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -35,6 +34,7 @@ import ru.inovus.ms.rdm.service.api.VersionFileService;
 import ru.inovus.ms.rdm.service.api.VersionService;
 import ru.inovus.ms.rdm.util.FileNameGenerator;
 import ru.inovus.ms.rdm.util.TimeUtils;
+import ru.inovus.ms.rdm.validation.ReferenceValidation;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -52,8 +52,7 @@ public class VersionServiceImpl implements VersionService {
 
     private static final String ROW_NOT_FOUND_EXCEPTION_CODE = "row.not.found";
     private static final String VERSION_NOT_FOUND_EXCEPTION_CODE = "version.not.found";
-    private static final String LAST_PUBLISHED_NOT_FOUND_EXCEPTION_CODE = "last.published.not.found";
-    private static final String PUBLISHED_DATA_NOT_FOUND = "published.data.not.found";
+    private static final String ACTUAL_DATA_NOT_FOUND = "actual.data.not.found";
 
     private RefBookVersionRepository versionRepository;
 
@@ -113,7 +112,7 @@ public class VersionServiceImpl implements VersionService {
     public RefBookVersion getLastPublishedVersion(String refBookCode) {
         RefBookVersionEntity versionEntity = versionRepository.findFirstByRefBookCodeAndStatusOrderByFromDateDesc(refBookCode, RefBookVersionStatus.PUBLISHED);
         if (versionEntity == null)
-            throw new NotFoundException(new Message(LAST_PUBLISHED_NOT_FOUND_EXCEPTION_CODE, refBookCode));
+            throw new NotFoundException(new Message(ReferenceValidation.LAST_PUBLISHED_NOT_FOUND_EXCEPTION_CODE, refBookCode));
         return versionModel(versionEntity);
     }
 
@@ -121,7 +120,7 @@ public class VersionServiceImpl implements VersionService {
     public Page<RefBookRowValue> search(String refBookCode, LocalDateTime date, SearchDataCriteria criteria) {
         RefBookVersionEntity version = versionRepository.findActualOnDate(refBookCode, date);
         if (version == null) {
-            throw new NotFoundException(new Message(PUBLISHED_DATA_NOT_FOUND));
+            throw new NotFoundException(new Message(ACTUAL_DATA_NOT_FOUND));
         }
         return getRowValuesOfVersion(criteria, version);
     }
