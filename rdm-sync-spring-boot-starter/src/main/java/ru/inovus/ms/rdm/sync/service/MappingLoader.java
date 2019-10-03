0d33b3ce608b0c8461dd8841214d24abc1e76250
Timur Nurdinov <tnurdinov@i-novus.ru>
2019-10-03 14:41:29 +0300

    bf RDM-555 sonar issues fix

--- rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/service/MappingLoader.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/service/MappingLoader.java
@@ -1,18 +1,6 @@
 package ru.inovus.ms.rdm.sync.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.transaction.annotation.Transactional;
-import ru.inovus.ms.rdm.api.exception.RdmException;
-import ru.inovus.ms.rdm.sync.model.loader.XmlMapping;
-import ru.inovus.ms.rdm.sync.model.loader.XmlMappingRefBook;
-
 import javax.annotation.PostConstruct;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class MappingLoader {
 
