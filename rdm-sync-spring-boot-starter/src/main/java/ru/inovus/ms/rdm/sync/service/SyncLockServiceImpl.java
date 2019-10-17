88de33c431f4dc6902a464b88af2a2f7de64a4ee
ikarimullin <ilyaskarimullin1997@gmail.com>
2019-10-17 11:01:08 +0300

    nf RDM-568 Сонар не пускает.

--- rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/service/SyncLockServiceImpl.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/service/SyncLockServiceImpl.java
@@ -1,6 +1,9 @@
 package ru.inovus.ms.rdm.sync.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.CannotAcquireLockException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.Timestamp;
 
 public class SyncLockServiceImpl {
+
+    private static final Logger logger = LoggerFactory.getLogger(RdmSyncRestImpl.class);
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -27,7 +32,8 @@ public class SyncLockServiceImpl {
             try {
                 jdbcTemplate.queryForObject("SELECT last_acquired FROM rdm_sync.sync_lock_table FOR UPDATE NOWAIT", Timestamp.class);
                 acquired = true;
-            } catch (Exception ex) {
+            } catch (CannotAcquireLockException ex) {
+                 logger.info("Cannot acquire lock.", ex);
                 acquired = false;
             }
             if (acquired) {
