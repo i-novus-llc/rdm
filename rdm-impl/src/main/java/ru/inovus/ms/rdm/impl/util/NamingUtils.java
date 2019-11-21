2c67041ed19e97cafe509ee6afc560a284e66a37
ikarimullin <ilyaskarimullin1997@gmail.com>
2019-11-21 16:58:46 +0300

    bf RDM-565 Нет проверки кода справочника в паспорте.

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/impl/util/NamingUtils.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/impl/util/NamingUtils.java
@@ -6,9 +6,10 @@ import java.util.regex.Pattern;
 
 public final class NamingUtils {
 
+    private static final Pattern REFBOOK_CODE = Pattern.compile("[A-Za-z][0-9A-Za-z\\-._]{0,49}");
+
     private NamingUtils() {throw new UnsupportedOperationException();}
 
-    private static final Pattern REFBOOK_CODE = Pattern.compile("[A-Za-z][0-9A-Za-z\\-._]{0,49}");
     public static void checkRefBookCode(String name) {
         if (!REFBOOK_CODE.matcher(name).matches())
             throw new UserException("refbook.name-not-valid");
