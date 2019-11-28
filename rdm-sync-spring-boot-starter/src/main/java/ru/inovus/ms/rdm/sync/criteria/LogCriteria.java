e5dd7bde680810b75e00356c85fbb8d592138e1c
ilebedenko <ilebedenko@i-novus.ru>
2019-11-28 12:01:25 +0300

    [BF][EPMP-857] - убран сваггер из rdm-starter api

--- rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/criteria/LogCriteria.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/criteria/LogCriteria.java
@@ -1,7 +1,5 @@
 package ru.inovus.ms.rdm.sync.criteria;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import ru.inovus.ms.rdm.api.model.AbstractCriteria;
@@ -16,13 +14,19 @@ import java.time.LocalDate;
  */
 @Getter
 @Setter
-@ApiModel("Критерии поиска записи журнала")
+/**
+ * Критерии поиска записи журнала
+ */
 public class LogCriteria extends AbstractCriteria {
-    @ApiModelProperty("Код справочника")
+    /**
+     * Код справочника
+     */
     @QueryParam("refbookCode")
     private String refbookCode;
 
-    @ApiModelProperty("Дата записи журнала")
+    /**
+     * Дата записи журнала
+     */
     @QueryParam("date")
     @NotNull
     private LocalDate date;
--- rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/rest/RdmSyncRest.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/inovus/ms/rdm/sync/rest/RdmSyncRest.java
@@ -1,7 +1,5 @@
 package ru.inovus.ms.rdm.sync.rest;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import ru.inovus.ms.rdm.api.model.refbook.RefBook;
 import ru.inovus.ms.rdm.sync.criteria.LogCriteria;
 import ru.inovus.ms.rdm.sync.model.Log;
@@ -15,26 +13,26 @@ import java.util.List;
  * @author lgalimova
  * @since 20.02.2019
  */
+
+/**
+ * Интерфейс API для синхронизации данных справочников НСИ
+ */
 @Path("rdm")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Api("Синхронизация данных справочников НСИ")
 public interface RdmSyncRest {
 
     @POST
     @Path("/update")
-    @ApiOperation(value = "Синхронизация всех справочников")
     void update();
 
     @POST
     @Path("/update/{refbookCode}")
-    @ApiOperation(value = "Синхронизация отдельного справочника")
     void update(@PathParam("refbookCode") String refbookCode);
 
     void update(RefBook refBook, VersionMapping versionMapping);
 
     @GET
     @Path("/log")
-    @ApiOperation(value = "Получение журнала за дату")
     List<Log> getLog(@BeanParam LogCriteria criteria);
 }
