cde6e97505640963ff771131c3263d81567fec09
ikarimullin <ikarimullin@i-novus.ru>
2020-01-17 18:35:59 +0300

    nf RDM-432 Правки

--- rdm-api/src/main/java/ru/inovus/ms/rdm/api/model/refdata/Row.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/api/model/refdata/Row.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Row implements Serializable {
 
     private Long systemId;
-    private Map<String, Object> data; // NOSONAR
+    private Map<String, Object> data;
 
     public Row() {}
 
--- rdm-impl/src/main/java/ru/inovus/ms/rdm/impl/async/AsyncOperationMessage.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/impl/async/AsyncOperationMessage.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 class AsyncOperationMessage implements Serializable {
 
-    private Object[] args; // NOSONAR
+    private Object[] args;
     private String userName;
     private UUID operationId;
     private AsyncOperation operation;
