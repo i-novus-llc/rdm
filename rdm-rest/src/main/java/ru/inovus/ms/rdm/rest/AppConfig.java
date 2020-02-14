4899d60287dd85514e16cbf60b5db521dadc5918
arahmatullin <arahmatullin@i-novus.ru>
2020-02-14 13:17:10 +0300

    сf RDM-730 remove proxy using from config.

--- rdm-rest/src/main/java/ru/inovus/ms/rdm/rest/AppConfig.java
+++ rdm-rest/src/main/java/ru/inovus/ms/rdm/rest/AppConfig.java
@@ -1,6 +1,5 @@
 package ru.inovus.ms.rdm.rest;
 
-import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
 import net.n2oapp.platform.loader.server.ServerLoader;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -8,20 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import ru.inovus.ms.rdm.api.service.DraftService;
-import ru.inovus.ms.rdm.api.service.FileStorageService;
-import ru.inovus.ms.rdm.api.service.PublishService;
-import ru.inovus.ms.rdm.api.service.RefBookService;
 import ru.inovus.ms.rdm.rest.loader.RefBookDataServerLoaderRunner;
 
 import java.util.List;
 
 @Configuration
 @ComponentScan({"ru.inovus.ms.rdm.rest", "net.n2oapp.platform.loader.server"})
-@EnableJaxRsProxyClient(
-    classes = {RefBookService.class, FileStorageService.class, PublishService.class},
-    address = "${rdm.backend.path}"
-)
 @SuppressWarnings("unused")
 public class AppConfig {
 
