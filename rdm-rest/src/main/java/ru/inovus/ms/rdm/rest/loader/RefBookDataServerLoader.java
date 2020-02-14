041307b12ef16c744769358f3b5e7cc4c617c2e2
arahmatullin <arahmatullin@i-novus.ru>
2020-02-14 11:11:00 +0300

    сf RDM-730 skip loading existing refbooks.

--- rdm-rest/src/main/java/ru/inovus/ms/rdm/rest/loader/RefBookDataServerLoader.java
+++ rdm-rest/src/main/java/ru/inovus/ms/rdm/rest/loader/RefBookDataServerLoader.java
@@ -1,5 +1,6 @@
 package ru.inovus.ms.rdm.rest.loader;
 
+import net.n2oapp.platform.jaxrs.RestException;
 import net.n2oapp.platform.loader.server.ServerLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,11 @@ import static org.apache.cxf.common.util.CollectionUtils.isEmpty;
 public class RefBookDataServerLoader implements ServerLoader<FileModel> {
 
     private static final Logger logger = LoggerFactory.getLogger(RefBookDataServerLoader.class);
+
+    private static final String REF_BOOK_ALREADY_EXISTS_EXCEPTION_CODE = "refbook.already.exists";
+    public static final String LOG_REF_BOOK_IS_ALREADY_EXISTS_SKIP_CREATE = "RefBook is already exists. Skip create from file '{}'";
+    public static final String LOG_ERROR_CREATING_AND_PUBLISHING_REF_BOOK = "Error creating and publishing refBook from file '{}'";
+    public static final String LOG_ERROR_DATA_LOADING_WITH_EXCEPTION = "Error data loading from file '%s':";
 
     @Autowired
     @Qualifier("refBookServiceJaxRsProxyClient")
@@ -41,17 +47,42 @@ public class RefBookDataServerLoader implements ServerLoader<FileModel> {
     @Override
     public void load(List<FileModel> data, String subject) {
 
-        if (isEmpty(data))
+        if (isEmpty(data)) {
+            logger.info("No data loading from subject = {}", subject);
             return;
+        }
 
-        data.forEach(this::createAndPublishRefBook);
+        logger.info("Start data loading from subject = {}, {} file(s)", subject, data.size());
+        try {
+            data.forEach(this::createAndPublishRefBook);
 
-        logger.info("subject = {}", subject);
+            logger.info("Finish data loading from subject = {}", subject);
+
+        } catch (Exception e) {
+            logger.error("Error data loading from subject = {}", subject);
+            throw e;
+        }
     }
 
     private void createAndPublishRefBook(FileModel fileModel) {
 
-        Draft draft = refBookService.create(fileModel);
-        publishService.publish(draft.getId(), null, null, null, false);
+        logger.info("Start data loading from file '{}'", fileModel.getName());
+        try {
+            Draft draft = refBookService.create(fileModel);
+            publishService.publish(draft.getId(), null, null, null, false);
+
+            logger.info("Finish data loading from file '{}'", fileModel.getName());
+
+        } catch (RestException e) {
+            if (REF_BOOK_ALREADY_EXISTS_EXCEPTION_CODE.equals(e.getMessage())) {
+                logger.info(LOG_REF_BOOK_IS_ALREADY_EXISTS_SKIP_CREATE, fileModel.getName());
+
+            } else {
+                logger.error(LOG_ERROR_CREATING_AND_PUBLISHING_REF_BOOK, fileModel.getName());
+                String errorMsg = String.format(LOG_ERROR_DATA_LOADING_WITH_EXCEPTION, fileModel.getName());
+                logger.error(errorMsg, e);
+                throw e;
+            }
+        }
     }
 }
