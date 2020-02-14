7b069903349e44a5acd59af96eca6809118f743e
arahmatullin <arahmatullin@i-novus.ru>
2020-02-14 13:17:34 +0300

    nf RDM-730 add `rdm-loader-client`.

---
+++ rdm-loader-client/src/main/java/ru/inovus/ms/rdm/loader/client/RefBookDataClientLoader.java
@@ -0,0 +1,39 @@
+package ru.inovus.ms.rdm.loader.client;
+
+import net.n2oapp.platform.loader.client.ClientLoader;
+import net.n2oapp.platform.loader.client.RestClientLoader;
+import org.springframework.core.io.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestOperations;
+
+@Component
+public class RefBookDataClientLoader extends RestClientLoader<MultiValueMap<String, Object>> implements ClientLoader {
+
+    public RefBookDataClientLoader(RestOperations restTemplate) {
+        super(restTemplate);
+    }
+
+    public RefBookDataClientLoader(RestOperations restTemplate, String endpointPattern) {
+        super(restTemplate, endpointPattern);
+    }
+
+    @Override
+    protected MultiValueMap<String, String> getHeaders() {
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>(1);
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE);
+        return headers;
+    }
+
+    @Override
+    protected MultiValueMap<String, Object> getData(Resource file) {
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", file);
+        return body;
+    }
+}
