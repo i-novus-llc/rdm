17ab123aa0832607867747d92f6b3be2bb6c4ac2
Timur Nurdinov <tnurdinov@i-novus.ru>
2020-03-04 11:39:27 +0300

    bf EPMP-1165 НСИ. Ошибка при публикации справочника. Забито хранилище сообщениями

--- rdm-rest/src/main/java/ru/inovus/ms/rdm/rest/BackendConfiguration.java
+++ rdm-rest/src/main/java/ru/inovus/ms/rdm/rest/BackendConfiguration.java
@@ -94,6 +94,7 @@ public class BackendConfiguration {
     public JmsTemplate topicJmsTemplate(ConnectionFactory connectionFactory) {
         JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
         jmsTemplate.setPubSubDomain(true);
+        jmsTemplate.setExplicitQosEnabled(true);
         long oneHour = 60000*60;
         jmsTemplate.setTimeToLive(oneHour);
         return jmsTemplate;
