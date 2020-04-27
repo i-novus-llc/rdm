272106357f44750a6e0552ab77c586bbd420ce55
arahmatullin <arahmatullin@i-novus.ru>
2020-04-27 09:02:54 +0300

    bf EPMP-1250 определение всех конфликтов только для последней опубликованной версии.

--- rdm-impl/src/main/java/ru/inovus/ms/rdm/impl/repository/RefBookModelDataRepository.java
+++ rdm-impl/src/main/java/ru/inovus/ms/rdm/impl/repository/RefBookModelDataRepository.java
@@ -11,47 +11,65 @@ public interface RefBookModelDataRepository extends
         JpaRepository<RefBookModelData, Integer>,
         QuerydslPredicateExecutor<RefBookModelData> {
 
-    @SuppressWarnings("squid:S1192")
-    String FIND_CONFLICT_DATA = "select \n" +
-            "  :referrerVersionId as referrer_version_id, \n" +
-            "  null as draft_version_id, \n" +
-            "  null as last_published_version_id, \n" +
-            "\n" +
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = :referrerVersionId \n" +
-            "       and c.ref_recordid is not null \n" +
-            "  ) as has_data_conflict, \n" +
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = :referrerVersionId \n" +
-            "       and c.conflict_type = 'UPDATED' \n" +
-            "  ) as has_updated_conflict, \n" +
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = :referrerVersionId \n" +
-            "       and c.conflict_type = 'ALTERED' \n" +
-            "  ) as has_altered_conflict, \n" +
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = :referrerVersionId \n" +
-            "       and c.ref_recordid is null \n" +
-            "  ) as has_structure_conflict, \n" +
-            "\n" +
-            "  (:hasLastPublishedReferrerVersion and \n" +
-            "   exists( \n" +
-            "     select 1 from n2o_rdm_management.ref_book_conflict c \n" +
+    String INNER_JOIN_REFERRED =
             "      inner join n2o_rdm_management.ref_book_version pv \n" +
-            "         on pv.id = c.published_id \n" +
-            "      where c.referrer_id = :lastPublishedReferrerVersionId \n" +
-                         // Last published referred version only:
+            "         on pv.id = c.published_id \n";
+
+    String AND_REFERRED_IS_LAST_PUBLISHED_ONLY =
             "        and c.published_id = ( \n" +
             "            select lv.id \n" +
             "              from n2o_rdm_management.ref_book_version lv \n" +
             "             where lv.ref_book_id = pv.ref_book_id \n" +
             "               and lv.status = 'PUBLISHED' \n" +
             "             order by lv.from_date desc \n" +
-            "             limit 1 )\n" +
+            "             limit 1 )\n";
+
+    @SuppressWarnings("squid:S1192")
+    String FIND_CONFLICT_DATA = "select \n" +
+            "  :referrerVersionId as referrer_version_id, \n" +
+            "  null as draft_version_id, \n" +
+            "  null as last_published_version_id, \n" +
+            "\n" +
+
+            "  exists( \n" +
+            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
+            INNER_JOIN_REFERRED +
+            "     where c.referrer_id = :referrerVersionId \n" +
+            "       and c.ref_recordid is not null \n" +
+            AND_REFERRED_IS_LAST_PUBLISHED_ONLY +
+            "  ) as has_data_conflict, \n" +
+
+            "  exists( \n" +
+            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
+            INNER_JOIN_REFERRED +
+            "     where c.referrer_id = :referrerVersionId \n" +
+            "       and c.conflict_type = 'UPDATED' \n" +
+            AND_REFERRED_IS_LAST_PUBLISHED_ONLY +
+            "  ) as has_updated_conflict, \n" +
+
+            "  exists( \n" +
+            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
+            INNER_JOIN_REFERRED +
+            "     where c.referrer_id = :referrerVersionId \n" +
+            "       and c.conflict_type = 'ALTERED' \n" +
+            AND_REFERRED_IS_LAST_PUBLISHED_ONLY +
+            "  ) as has_altered_conflict, \n" +
+
+            "  exists( \n" +
+            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
+            INNER_JOIN_REFERRED +
+            "     where c.referrer_id = :referrerVersionId \n" +
+            "       and c.ref_recordid is null \n" +
+            AND_REFERRED_IS_LAST_PUBLISHED_ONLY +
+            "  ) as has_structure_conflict, \n" +
+
+            "\n" +
+            "  (:hasLastPublishedReferrerVersion and \n" +
+            "   exists( \n" +
+            "     select 1 from n2o_rdm_management.ref_book_conflict c \n" +
+            INNER_JOIN_REFERRED +
+            "      where c.referrer_id = :lastPublishedReferrerVersionId \n" +
+            AND_REFERRED_IS_LAST_PUBLISHED_ONLY +
             "   )\n" +
             "  ) as last_has_conflict \n";
 
