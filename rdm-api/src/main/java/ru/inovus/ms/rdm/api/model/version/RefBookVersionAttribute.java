1617ec2316c1a60e4ee1dfbf529aeaa01931c454
arahmatullin <arahmatullin@i-novus.ru>
2020-07-06 08:50:12 +0300

    cf RDM-489 уточнение CreateAttributeRequest.

--- rdm-api/src/main/java/ru/inovus/ms/rdm/api/model/version/RefBookVersionAttribute.java
+++ rdm-api/src/main/java/ru/inovus/ms/rdm/api/model/version/RefBookVersionAttribute.java
@@ -2,15 +2,18 @@ package ru.inovus.ms.rdm.api.model.version;
 
 import io.swagger.annotations.ApiModelProperty;
 import ru.inovus.ms.rdm.api.model.Structure;
+import ru.inovus.ms.rdm.api.model.refdata.DraftChangeRequest;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 /** Модель атрибута версии справочника. */
-public class RefBookVersionAttribute implements Serializable {
+public class RefBookVersionAttribute implements DraftChangeRequest {
 
     @ApiModelProperty("Идентификатор версии")
     private Integer versionId;
+
+    @ApiModelProperty("Значение оптимистической блокировки версии-черновика")
+    private Integer optLockValue;
 
     @ApiModelProperty("Атрибут версии")
     private Structure.Attribute attribute;
@@ -29,12 +32,32 @@ public class RefBookVersionAttribute implements Serializable {
         this.reference = reference;
     }
 
+    public RefBookVersionAttribute(Integer versionId,
+                                  Integer optLockValue,
+                                  Structure.Attribute attribute,
+                                  Structure.Reference reference) {
+        this(versionId, attribute, reference);
+        this.optLockValue = optLockValue;
+    }
+
+    @Override
     public Integer getVersionId() {
         return versionId;
     }
 
+    @Override
     public void setVersionId(Integer versionId) {
         this.versionId = versionId;
+    }
+
+    @Override
+    public Integer getOptLockValue() {
+        return optLockValue;
+    }
+
+    @Override
+    public void setOptLockValue(Integer optLockValue) {
+        this.optLockValue = optLockValue;
     }
 
     public Structure.Attribute getAttribute() {
