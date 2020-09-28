8f64ad6f8313d2e50544d24f24e3c4200aba866b
arahmatullin <arahmatullin@i-novus.ru>
2020-09-28 14:38:12 +0300

    nf RDM-782 добавление тестового покрытия `L10nLocalizeVersionControllerTest`.

---
+++ rdm-n2o-l10n/src/test/java/ru/i_novus/ms/rdm/n2o/l10n/service/L10nLocalizeVersionControllerTest.java
@@ -0,0 +1,109 @@
+package ru.i_novus.ms.rdm.n2o.l10n.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.n2oapp.platform.i18n.UserException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.i_novus.ms.rdm.api.model.refdata.Row;
+import ru.i_novus.ms.rdm.api.service.l10n.L10nVersionStorageService;
+import ru.i_novus.ms.rdm.api.util.json.JsonUtil;
+import ru.i_novus.ms.rdm.l10n.api.model.LocalizeDataRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static ru.i_novus.ms.rdm.n2o.l10n.utils.UiL10nRefBookTestUtils.getExceptionMessage;
+import static ru.i_novus.ms.rdm.n2o.l10n.utils.UiL10nRefBookTestUtils.getFailedMessage;
+
+@RunWith(MockitoJUnitRunner.class)
+public class L10nLocalizeVersionControllerTest {
+
+    private static final int TEST_REFBOOK_VERSION_ID = -10;
+    private static final int TEST_OPT_LOCK_VALUE = 10;
+    private static final String TEST_REFBOOK_CODE = "L10N_TEST";
+
+    private static final String TEST_LOCALE_CODE = "test";
+
+    private static final String TEST_FIELD_CODE = "id";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @InjectMocks
+    private L10nLocalizeVersionController controller;
+
+    @Mock
+    private L10nVersionStorageService versionStorageService;
+
+    @Before
+    @SuppressWarnings("java:S2696")
+    public void setUp() {
+        JsonUtil.jsonMapper = objectMapper;
+    }
+
+    @Test
+    public void testLocalizeDataRecord() {
+
+        Map<String, Object> data = new HashMap<>(1);
+        data.put(TEST_FIELD_CODE, 10);
+
+        Row row = new Row(1L, data);
+
+        LocalizeDataRequest expected = new LocalizeDataRequest(TEST_OPT_LOCK_VALUE, TEST_LOCALE_CODE, singletonList(row));
+
+        controller.localizeDataRecord(TEST_REFBOOK_VERSION_ID, TEST_OPT_LOCK_VALUE, TEST_LOCALE_CODE, row);
+
+        ArgumentCaptor<LocalizeDataRequest> captor = ArgumentCaptor.forClass(LocalizeDataRequest.class);
+        verify(versionStorageService, times(1))
+                .localizeData(eq(TEST_REFBOOK_VERSION_ID), captor.capture());
+
+        LocalizeDataRequest actual = captor.getValue();
+        assertEquals(expected.getOptLockValue(), actual.getOptLockValue());
+        assertEquals(expected.getLocaleCode(), actual.getLocaleCode());
+        assertEquals(expected.getRows().size(), actual.getRows().size());
+        assertEquals(expected.getRows().get(0).getSystemId(), actual.getRows().get(0).getSystemId());
+        assertEquals(
+                expected.getRows().get(0).getData().get(TEST_FIELD_CODE),
+                actual.getRows().get(0).getData().get(TEST_FIELD_CODE)
+        );
+    }
+
+    @Test
+    public void testLocalizeDataRecordFailed() {
+
+        testLocalizeDataRecordFailed(null);
+
+        testLocalizeDataRecordFailed(new Row(1L, null));
+
+        Map<String, Object> data = new HashMap<>(1);
+        Row row = new Row(1L, data);
+        testLocalizeDataRecordFailed(row);
+
+        data.put(TEST_FIELD_CODE, null);
+        testLocalizeDataRecordFailed(row);
+
+        data.put(TEST_FIELD_CODE, "");
+        testLocalizeDataRecordFailed(row);
+    }
+
+    private void testLocalizeDataRecordFailed(Row row) {
+        try {
+            controller.localizeDataRecord(TEST_REFBOOK_VERSION_ID, TEST_OPT_LOCK_VALUE, TEST_LOCALE_CODE, row);
+            fail(getFailedMessage(UserException.class));
+
+        } catch (RuntimeException e) {
+            assertEquals(UserException.class, e.getClass());
+            assertEquals("data.row.is.empty", getExceptionMessage(e));
+        }
+    }
+}
