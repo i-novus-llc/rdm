8002a0252462d49989b5267ca97ec0fa6d4c5a45
arahmatullin <arahmatullin@i-novus.ru>
2020-09-28 12:18:51 +0300

    cf RDM-782 добавление тестов для асинхронных резолверов.

---
+++ rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/async/AsyncPublishResolverTest.java
@@ -0,0 +1,58 @@
+package ru.i_novus.ms.rdm.impl.async;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.i_novus.ms.rdm.api.async.AsyncOperationTypeEnum;
+import ru.i_novus.ms.rdm.api.model.draft.PublishRequest;
+import ru.i_novus.ms.rdm.api.service.PublishService;
+
+import java.io.Serializable;
+
+import static org.junit.Assert.*;
+import static ru.i_novus.ms.rdm.impl.util.RefBookTestUtils.getExceptionMessage;
+import static ru.i_novus.ms.rdm.impl.util.RefBookTestUtils.getFailedMessage;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AsyncPublishResolverTest {
+
+    private static final String TEST_REF_BOOK_CODE = "test";
+    private static final int TEST_REFBOOK_VERSION_ID = -10;
+
+    @InjectMocks
+    private AsyncPublishResolver resolver;
+
+    @Mock
+    private PublishService publishService;
+
+    @Test
+    public void testIsSatisfied() {
+
+        assertTrue(resolver.isSatisfied(AsyncOperationTypeEnum.PUBLICATION));
+        assertFalse(resolver.isSatisfied(null));
+    } 
+
+    @Test
+    public void testResolve() {
+
+        Serializable result = resolver.resolve(TEST_REF_BOOK_CODE,
+                new Serializable[]{TEST_REFBOOK_VERSION_ID, new PublishRequest()}
+        );
+        assertNull(result);
+    }
+
+    @Test
+    public void testResolveFailed() {
+
+        try {
+            resolver.resolve(TEST_REF_BOOK_CODE, new Serializable[]{TEST_REFBOOK_VERSION_ID, null});
+            fail(getFailedMessage(IllegalArgumentException.class));
+
+        } catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+            assertNotNull(getExceptionMessage(e));
+        }
+    }
+}
