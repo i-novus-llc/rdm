0731de5b9da6bffdaf129b597dc4c4ef2038fc02
arahmatullin <arahmatullin@i-novus.ru>
2020-09-29 15:10:26 +0300

    cf RDM-782 выделение констант структуры версии в отдельный класс.

--- rdm-api/src/test/java/ru/i_novus/ms/rdm/api/model/StructureTest.java
+++ rdm-api/src/test/java/ru/i_novus/ms/rdm/api/model/StructureTest.java
@@ -8,89 +8,15 @@ import org.junit.Test;
 import ru.i_novus.ms.rdm.api.util.RefBookTestUtils;
 import ru.i_novus.ms.rdm.api.util.json.JsonUtil;
 import ru.i_novus.platform.datastorage.temporal.enums.FieldType;
-import ru.i_novus.platform.datastorage.temporal.model.DisplayExpression;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
 import static ru.i_novus.ms.rdm.api.util.RefBookTestUtils.*;
+import static ru.i_novus.ms.rdm.api.util.StructureTestConstants.*;
 
 public class StructureTest {
-
-    private static final String ID_ATTRIBUTE_CODE = "ID";
-    private static final String NAME_ATTRIBUTE_CODE = "NAME";
-    private static final String STRING_ATTRIBUTE_CODE = "CHAR";
-    private static final String NUMBER_ATTRIBUTE_CODE = "NUMB";
-    private static final String BOOLEAN_ATTRIBUTE_CODE = "BOOL";
-    private static final String DATE_ATTRIBUTE_CODE = "DATE";
-
-    private static final List<String> ATTRIBUTE_CODES = List.of(
-            ID_ATTRIBUTE_CODE, NAME_ATTRIBUTE_CODE,
-            STRING_ATTRIBUTE_CODE, NUMBER_ATTRIBUTE_CODE, BOOLEAN_ATTRIBUTE_CODE, DATE_ATTRIBUTE_CODE
-    );
-
-    private static final String REFERENCE_ATTRIBUTE_CODE = "REFER";
-    private static final String SELF_REFER_ATTRIBUTE_CODE = "SELFY";
-
-    private static final List<String> REFERENCE_CODES = List.of(
-            REFERENCE_ATTRIBUTE_CODE, SELF_REFER_ATTRIBUTE_CODE
-    );
-
-    private static final String REFERRED_BOOK_CODE = "REFERRED";
-    private static final String REFERRED_BOOK_ATTRIBUTE_CODE = "VALUE";
-    private static final String SELF_REFERRED_BOOK_CODE = "SELFBOOK";
-    private static final String SELF_REFERRED_BOOK_ATTRIBUTE_CODE = ID_ATTRIBUTE_CODE;
-    private static final List<String> REFERRED_BOOK_CODES = List.of(
-            REFERRED_BOOK_CODE, SELF_REFERRED_BOOK_CODE
-    );
-
-    private static final Structure.Attribute ID_ATTRIBUTE = Structure.Attribute.buildPrimary(
-            ID_ATTRIBUTE_CODE, ID_ATTRIBUTE_CODE.toLowerCase(), FieldType.INTEGER, "primary");
-    private static final Structure.Attribute NAME_ATTRIBUTE = Structure.Attribute.buildLocalizable(
-            NAME_ATTRIBUTE_CODE, NAME_ATTRIBUTE_CODE.toLowerCase(), FieldType.STRING, "name");
-    private static final Structure.Attribute STRING_ATTRIBUTE = Structure.Attribute.build(
-            STRING_ATTRIBUTE_CODE, STRING_ATTRIBUTE_CODE.toLowerCase(), FieldType.STRING, "string");
-    private static final Structure.Attribute NUMBER_ATTRIBUTE = Structure.Attribute.build(
-            NUMBER_ATTRIBUTE_CODE, NUMBER_ATTRIBUTE_CODE.toLowerCase(), FieldType.INTEGER, "number");
-    private static final Structure.Attribute BOOLEAN_ATTRIBUTE = Structure.Attribute.build(
-            BOOLEAN_ATTRIBUTE_CODE, BOOLEAN_ATTRIBUTE_CODE.toLowerCase(), FieldType.BOOLEAN, "boolean");
-    private static final Structure.Attribute DATE_ATTRIBUTE = Structure.Attribute.build(
-            DATE_ATTRIBUTE_CODE, DATE_ATTRIBUTE_CODE.toLowerCase(), FieldType.DATE, "date");
-    private static final Structure.Attribute REFERENCE_ATTRIBUTE = Structure.Attribute.build(
-            REFERENCE_ATTRIBUTE_CODE, REFERENCE_ATTRIBUTE_CODE.toLowerCase(), FieldType.REFERENCE, "reference");
-    private static final Structure.Attribute SELF_REFER_ATTRIBUTE = Structure.Attribute.build(
-            SELF_REFER_ATTRIBUTE_CODE, SELF_REFER_ATTRIBUTE_CODE.toLowerCase(), FieldType.REFERENCE, "self-ref");
-
-    private static final Structure.Reference REFERENCE = new Structure.Reference(
-            REFERENCE_ATTRIBUTE_CODE, REFERRED_BOOK_CODE,
-            DisplayExpression.toPlaceholder(REFERRED_BOOK_ATTRIBUTE_CODE)
-    );
-    private static final Structure.Reference SELF_REFER = new Structure.Reference(
-            SELF_REFER_ATTRIBUTE_CODE, SELF_REFERRED_BOOK_CODE,
-            DisplayExpression.toPlaceholder(SELF_REFERRED_BOOK_ATTRIBUTE_CODE)
-    );
-
-    private static final List<Structure.Attribute> ATTRIBUTE_LIST = List.of(
-            ID_ATTRIBUTE, NAME_ATTRIBUTE,
-            STRING_ATTRIBUTE, NUMBER_ATTRIBUTE, BOOLEAN_ATTRIBUTE, DATE_ATTRIBUTE,
-            REFERENCE_ATTRIBUTE, SELF_REFER_ATTRIBUTE
-    );
-    private static final List<Structure.Reference> REFERENCE_LIST = List.of(
-            REFERENCE, SELF_REFER
-    );
-
-    private static final String CHANGE_ATTRIBUTE_CODE = "CHANGE";
-    private static final Structure.Attribute CHANGE_ATTRIBUTE = Structure.Attribute.build(
-            CHANGE_ATTRIBUTE_CODE, CHANGE_ATTRIBUTE_CODE.toLowerCase(), FieldType.STRING, "change");
-
-    private static final String CHANGE_REF_ATTRIBUTE_CODE = "CHANGE_REF";
-    private static final Structure.Attribute CHANGE_REF_ATTRIBUTE = Structure.Attribute.build(
-            CHANGE_REF_ATTRIBUTE_CODE, CHANGE_REF_ATTRIBUTE_CODE.toLowerCase(), FieldType.REFERENCE, "change-ref");
-    private static final Structure.Reference CHANGE_REF_REFERENCE = new Structure.Reference(
-            CHANGE_REF_ATTRIBUTE_CODE, REFERRED_BOOK_CODE,
-            DisplayExpression.toPlaceholder(REFERRED_BOOK_ATTRIBUTE_CODE)
-    );
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
