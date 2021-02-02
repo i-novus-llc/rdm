da3d26c195060126c7f0c56f8524f9ea710e16a6
arahmatullin <arahmatullin@i-novus.ru>
2021-02-02 12:30:44 +0300

    nf RDM-815 Добавление формирования объекта из json-строки.

--- rdm-api/src/main/java/ru/i_novus/ms/rdm/api/util/json/JsonUtil.java
+++ rdm-api/src/main/java/ru/i_novus/ms/rdm/api/util/json/JsonUtil.java
@@ -3,6 +3,8 @@ package ru.i_novus.ms.rdm.api.util.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 public final class JsonUtil {
 
     public static ObjectMapper jsonMapper; // NOSONAR
@@ -17,6 +19,15 @@ public final class JsonUtil {
 
         } catch (JsonProcessingException e) {
             throw new IllegalArgumentException("Cannot serialize json value.", e);
+        }
+    }
+
+    public static <T> T fromJsonString(String value, Class<T> clazz) {
+        try {
+            return jsonMapper.readValue(value, clazz);
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Cannot deserialize json value.", e);
         }
     }
 }
