bc7f97c9179a06bcd78061187654b97e69bed386
arahmatullin <arahmatullin@i-novus.ru>
2021-02-02 12:29:42 +0300

    nf RDM-815 Сущность "Разница между данными версий".

---
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/entity/diff/VersionDataDiffEntity.java
@@ -0,0 +1,87 @@
+package ru.i_novus.ms.rdm.impl.entity.diff;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "version_data_diff", schema = "diff")
+@TypeDef(
+    name = "jsonb",
+    typeClass = JsonBinaryType.class
+)
+public class VersionDataDiffEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "version_diff_id", nullable = false)
+    private RefBookVersionDiffEntity versionDiffEntity;
+
+    @Type(type = "jsonb")
+    @Column(name = "primaries", nullable = false)
+    private String primaries;
+
+    @Type(type = "jsonb")
+    @Column(name = "values", nullable = false)
+    private String values;
+
+    public VersionDataDiffEntity() {
+        // Nothing to do.
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public RefBookVersionDiffEntity getVersionDiffEntity() {
+        return versionDiffEntity;
+    }
+
+    public void setVersionDiffEntity(RefBookVersionDiffEntity versionDiffEntity) {
+        this.versionDiffEntity = versionDiffEntity;
+    }
+
+    public String getPrimaries() {
+        return primaries;
+    }
+
+    public void setPrimaries(String primaries) {
+        this.primaries = primaries;
+    }
+
+    public String getValues() {
+        return values;
+    }
+
+    public void setValues(String values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VersionDataDiffEntity that = (VersionDataDiffEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(versionDiffEntity, that.versionDiffEntity) &&
+                Objects.equals(primaries, that.primaries) &&
+                Objects.equals(values, that.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, versionDiffEntity, primaries, values);
+    }
+}
