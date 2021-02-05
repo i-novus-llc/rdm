60efab284cc3dbff45d4ebb6035ca5acc87022ca
arahmatullin <arahmatullin@i-novus.ru>
2021-02-05 15:09:25 +0300

    cf RDM-815 Использование строки вместо jsonb для diff values.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/entity/diff/VersionDataDiffEntity.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/entity/diff/VersionDataDiffEntity.java
@@ -1,18 +1,10 @@
 package ru.i_novus.ms.rdm.impl.entity.diff;
 
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "version_data_diff", schema = "diff")
-@TypeDef(
-    name = "jsonb",
-    typeClass = JsonBinaryType.class
-)
 public class VersionDataDiffEntity {
 
     @Id
@@ -27,7 +19,6 @@ public class VersionDataDiffEntity {
     @Column(name = "primaries", nullable = false)
     private String primaries;
 
-    @Type(type = "jsonb")
     @Column(name = "values", nullable = false)
     private String values;
 
