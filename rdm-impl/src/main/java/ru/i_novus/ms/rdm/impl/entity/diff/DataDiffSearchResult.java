693de835509832ae226d50238bdf65be29a843ef
arahmatullin <arahmatullin@i-novus.ru>
2021-02-11 13:33:21 +0300

    cf RDM-821 Сущность для получения разницы одним запросом.

---
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/entity/diff/DataDiffSearchResult.java
@@ -0,0 +1,49 @@
+package ru.i_novus.ms.rdm.impl.entity.diff;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Разница между данными версий: Результат поиска.
+ * <p>
+ * Версии не обязательно следуют друг за другом.
+ * В этом случае нужны два diff: первый и последний.
+ */
+@Entity
+public class DataDiffSearchResult {
+
+    @Id
+    @Column(name = "primary_values")
+    private String primaryValues;
+
+    @Column(name = "first_diff_values")
+    private String firstDiffValues;
+
+    @Column(name = "last_diff_values")
+    private String lastDiffValues;
+
+    public String getPrimaryValues() {
+        return primaryValues;
+    }
+
+    public void setPrimaryValues(String primaryValues) {
+        this.primaryValues = primaryValues;
+    }
+
+    public String getFirstDiffValues() {
+        return firstDiffValues;
+    }
+
+    public void setFirstDiffValues(String firstDiffValues) {
+        this.firstDiffValues = firstDiffValues;
+    }
+
+    public String getLastDiffValues() {
+        return lastDiffValues;
+    }
+
+    public void setLastDiffValues(String lastDiffValues) {
+        this.lastDiffValues = lastDiffValues;
+    }
+}
