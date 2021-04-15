3c63696bfe1825a3c19fa63621ee615eba3d4a63
arahmatullin <arahmatullin@i-novus.ru>
2021-04-15 14:55:04 +0300

    cf RDM-828 Рефакторинг стратегии нумерации версий.

--- rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/util/SequenceVersionNumberStrategyTest.java
+++ rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/util/SequenceVersionNumberStrategyTest.java
@@ -6,17 +6,20 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import ru.i_novus.ms.rdm.impl.entity.RefBookVersionEntity;
 import ru.i_novus.ms.rdm.api.enumeration.RefBookVersionStatus;
 import ru.i_novus.ms.rdm.api.model.Structure;
+import ru.i_novus.ms.rdm.impl.entity.RefBookVersionEntity;
 import ru.i_novus.ms.rdm.impl.repository.RefBookVersionRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static ru.i_novus.ms.rdm.api.enumeration.RefBookVersionStatus.DRAFT;
+import static ru.i_novus.ms.rdm.api.enumeration.RefBookVersionStatus.PUBLISHED;
 
 /**
  * Created by znurgaliev on 13.08.2018.
@@ -25,10 +28,17 @@ import static org.mockito.Mockito.when;
 public class SequenceVersionNumberStrategyTest {
 
     @InjectMocks
-    SequenceVersionNumberStrategy versionNumberStrategy;
+    private SequenceVersionNumberStrategy strategy;
 
     @Mock
-    RefBookVersionRepository versionRepository;
+    private RefBookVersionRepository versionRepository;
+
+    @Test
+    public void testFirst() {
+
+        String version = strategy.first();
+        assertNotNull(version);
+    }
 
     @Test
     public void testNext() {
@@ -37,42 +47,45 @@ public class SequenceVersionNumberStrategyTest {
         Structure.Attribute attribute = new Structure.Attribute();
         Structure structure2 = new Structure(Collections.singletonList(attribute), new ArrayList<>());
 
-        RefBookVersionEntity e1 = createVersionEntity(RefBookVersionStatus.PUBLISHED, "1.0", structure2);
-        RefBookVersionEntity e2 = createVersionEntity(RefBookVersionStatus.PUBLISHED, "20.5", structure1);
-        RefBookVersionEntity e3 = createVersionEntity(RefBookVersionStatus.PUBLISHED, "14.1", structure2);
-        RefBookVersionEntity eqStructureDraft = createVersionEntity(RefBookVersionStatus.DRAFT, "0.0", structure1);
-        RefBookVersionEntity notEqStructureDraft = createVersionEntity(RefBookVersionStatus.DRAFT, null, structure2);
+        RefBookVersionEntity e1 = createVersionEntity(PUBLISHED, "1.0", structure2);
+        RefBookVersionEntity e2 = createVersionEntity(PUBLISHED, "20.5", structure1);
+        RefBookVersionEntity e3 = createVersionEntity(PUBLISHED, "14.1", structure2);
+        RefBookVersionEntity eqStructureDraft = createVersionEntity(DRAFT, "0.0", structure1);
+        RefBookVersionEntity notEqStructureDraft = createVersionEntity(DRAFT, null, structure2);
 
-        when(versionRepository.findAllByStatusAndRefBookId(eq(RefBookVersionStatus.PUBLISHED), eq(1)))
+        when(versionRepository.findAllByStatusAndRefBookId(eq(PUBLISHED), eq(1)))
                 .thenReturn(Arrays.asList(e1, e2, e3));
 
-        when(versionRepository.findByStatusAndRefBookId(eq(RefBookVersionStatus.DRAFT), eq(1)))
+        when(versionRepository.findByStatusAndRefBookId(eq(DRAFT), eq(1)))
                 .thenReturn(eqStructureDraft);
-        Assert.assertEquals("20.6", versionNumberStrategy.next(1));
+        Assert.assertEquals("20.6", strategy.next(1));
 
-        when(versionRepository.findByStatusAndRefBookId(eq(RefBookVersionStatus.DRAFT), eq(1)))
+        when(versionRepository.findByStatusAndRefBookId(eq(DRAFT), eq(1)))
                 .thenReturn(notEqStructureDraft);
-        Assert.assertEquals("21.0", versionNumberStrategy.next(1));
+        Assert.assertEquals("21.0", strategy.next(1));
 
     }
 
     @Test
     public void testCheck() {
-        RefBookVersionEntity e1 = createVersionEntity(RefBookVersionStatus.PUBLISHED, "1.0", null);
-        RefBookVersionEntity e2 = createVersionEntity(RefBookVersionStatus.PUBLISHED, "20.5", null);
-        RefBookVersionEntity e3 = createVersionEntity(RefBookVersionStatus.PUBLISHED, "14.1", null);
-        when(versionRepository.findAllByStatusAndRefBookId(eq(RefBookVersionStatus.PUBLISHED), eq(1)))
+
+        RefBookVersionEntity e1 = createVersionEntity(PUBLISHED, "1.0", null);
+        RefBookVersionEntity e2 = createVersionEntity(PUBLISHED, "20.5", null);
+        RefBookVersionEntity e3 = createVersionEntity(PUBLISHED, "14.1", null);
+        when(versionRepository.findAllByStatusAndRefBookId(eq(PUBLISHED), eq(1)))
                 .thenReturn(Arrays.asList(e1, e2, e3));
 
-        Assert.assertFalse(versionNumberStrategy.check("1.0", 1));
-        Assert.assertTrue(versionNumberStrategy.check("2.0", 1));
+        Assert.assertFalse(strategy.check("1.0", 1));
+        Assert.assertTrue(strategy.check("2.0", 1));
     }
 
     private RefBookVersionEntity createVersionEntity(RefBookVersionStatus status, String version, Structure structure){
+
         RefBookVersionEntity entity = new RefBookVersionEntity();
         entity.setStatus(status);
         entity.setVersion(version);
         entity.setStructure(structure);
+
         return entity;
     }
 }
