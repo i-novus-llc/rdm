4c8f2f60c1a8651fccd1b4ea02c3ea5a22fb4d4a
arahmatullin <arahmatullin@i-novus.ru>
2021-04-15 12:17:32 +0300

    cf RDM-828 Доработка стратегий неверсионных справочников.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/strategy/refbook/UnversionedCreateFirstDataDraftStrategy.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/strategy/refbook/UnversionedCreateFirstDataDraftStrategy.java
@@ -1,17 +1,17 @@
 package ru.i_novus.ms.rdm.impl.strategy.refbook;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ru.i_novus.ms.rdm.api.util.TimeUtils;
 
 @Component
 public class UnversionedCreateFirstDataDraftStrategy extends DefaultCreateFirstDataDraftStrategy {
 
     @Override
+    @Transactional
     public String create() {
 
-        String storageCode = super.create();
-        getDraftDataService().applyDraft(null, storageCode, TimeUtils.now());
-
-        return storageCode;
+        String draftCode = super.create();
+        return getDraftDataService().applyDraftItself(draftCode, TimeUtils.now());
     }
 }
--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/strategy/refbook/UnversionedCreateFirstVersionStrategy.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/strategy/refbook/UnversionedCreateFirstVersionStrategy.java
@@ -1,6 +1,8 @@
 package ru.i_novus.ms.rdm.impl.strategy.refbook;
 
+import ru.i_novus.ms.rdm.api.enumeration.RefBookVersionStatus;
 import ru.i_novus.ms.rdm.api.model.refbook.RefBookCreateRequest;
+import ru.i_novus.ms.rdm.api.util.TimeUtils;
 import ru.i_novus.ms.rdm.impl.entity.RefBookEntity;
 import ru.i_novus.ms.rdm.impl.entity.RefBookVersionEntity;
 
@@ -10,7 +12,9 @@ public class UnversionedCreateFirstVersionStrategy extends DefaultCreateFirstVer
     public RefBookVersionEntity create(RefBookEntity refBookEntity, RefBookCreateRequest request) {
 
         RefBookVersionEntity entity = super.create(refBookEntity, request);
+        entity.setStatus(RefBookVersionStatus.PUBLISHED);
         entity.setVersion("1.0"); // from SequenceVersionNumberStrategy
+        entity.setFromDate(TimeUtils.now());
 
         return entity;
     }
