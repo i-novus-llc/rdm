a3d8d093dd71221c4c8720ff1bf60dd22f18f7d4
arahmatullin <arahmatullin@i-novus.ru>
2021-04-16 11:58:38 +0300

    cf RDM-828 Значение по умолчанию для типа справочника.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/file/export/XmlFileGenerator.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/file/export/XmlFileGenerator.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.i_novus.ms.rdm.api.exception.RdmException;
 import ru.i_novus.ms.rdm.api.model.Structure;
+import ru.i_novus.ms.rdm.api.model.refbook.RefBookType;
 import ru.i_novus.ms.rdm.api.model.refdata.Row;
 import ru.i_novus.ms.rdm.api.model.validation.AttributeValidation;
 import ru.i_novus.ms.rdm.api.model.version.RefBookVersion;
@@ -20,17 +21,24 @@ public class XmlFileGenerator extends PerRowFileGenerator {
 
     private static final Logger logger = LoggerFactory.getLogger(XmlFileGenerator.class);
 
+    private static final String CODE_TAG_NAME = "code";
+    private static final String TYPE_TAG_NAME = "type";
+    private static final String PASSPORT_TAG_NAME = "passport";
+    private static final String STRUCTURE_TAG_NAME = "structure";
+    private static final String DATA_TAG_NAME = "data";
+    private static final String ROW_TAG_NAME = "row";
+
     private static final String XML_GENERATE_ERROR_MESSAGE = "cannot generate XML";
 
-    private Map<String, Structure.Reference> attributeToReferenceMap;
+    private final Map<String, Structure.Reference> attributeToReferenceMap;
+
+    private final Map<String, String> passport;
+
+    private final RefBookVersion version;
+
+    private final List<AttributeValidation> attributeValidations;
 
     private XMLStreamWriter writer;
-
-    private Map<String, String> passport;
-
-    private RefBookVersion version;
-
-    private List<AttributeValidation> attributeValidations;
 
     /**
      *
@@ -43,7 +51,9 @@ public class XmlFileGenerator extends PerRowFileGenerator {
                             RefBookVersion version,
                             Map<String, Structure.Reference> attributeToReferenceMap,
                             List<AttributeValidation> attributeValidations) {
+
         super(rowIterator, version.getStructure());
+
         this.passport = version.getPassport();
         this.attributeToReferenceMap = attributeToReferenceMap;
         this.attributeValidations = attributeValidations;
@@ -63,7 +73,8 @@ public class XmlFileGenerator extends PerRowFileGenerator {
             addPassport();
             addStructure();
 
-            writer.writeStartElement("data");
+            writer.writeStartElement(DATA_TAG_NAME);
+
         } catch (XMLStreamException e) {
             throwXmlGenerateError(e);
         }
@@ -72,12 +83,13 @@ public class XmlFileGenerator extends PerRowFileGenerator {
     @Override
     protected void write(Row row) {
         try {
-            writer.writeStartElement("row");
+            writer.writeStartElement(ROW_TAG_NAME);
             for (String fieldCode : getStructure().getAttributeCodes()) {
                 if (row.getData().get(fieldCode) != null) {
                     String stringValue = ConverterUtil.toString(row.getData().get(fieldCode));
                     if (stringValue == null) {
                         writer.writeEmptyElement(fieldCode);
+
                     } else {
                         writer.writeStartElement(fieldCode);
                         writer.writeCharacters(stringValue);
@@ -86,6 +98,7 @@ public class XmlFileGenerator extends PerRowFileGenerator {
                 }
             }
             writer.writeEndElement();
+
         } catch (XMLStreamException e) {
             throwXmlGenerateError(e);
         }
@@ -94,39 +107,46 @@ public class XmlFileGenerator extends PerRowFileGenerator {
     @Override
     protected void endWrite() {
         try {
-            writer.writeEndElement(); //data
-            writer.writeEndElement(); //refBook
+            writer.writeEndElement(); // DATA_TAG_NAME
+            writer.writeEndElement(); // refBook
             writer.writeEndDocument();
             writer.flush();
+
             logger.info("XML generate finished");
+
         } catch (XMLStreamException e) {
             throwXmlGenerateError(e);
         }
     }
 
     private void addPassport() throws XMLStreamException {
-        writer.writeStartElement("passport");
+
+        writer.writeStartElement(PASSPORT_TAG_NAME);
         passport.keySet().forEach(v -> {
             try {
                 writer.writeStartElement(v);
                 writer.writeCharacters(passport.get(v));
                 writer.writeEndElement();
+
             } catch (XMLStreamException e) {
                 throwXmlGenerateError(e);
             }
         });
-        writer.writeEndElement(); //passport
+        writer.writeEndElement();
     }
 
     private void addFields() {
 
-        writeElement("code", version.getCode());
-        writeElement("type",  version.getType() == null ? null : version.getType().name());
+        writeElement(CODE_TAG_NAME, version.getCode());
+
+        if (!RefBookType.DEFAULT.equals(version.getType())) {
+            writeElement(TYPE_TAG_NAME, version.getType().name());
+        }
     }
 
     private void addStructure() {
         try {
-            writer.writeStartElement("structure");
+            writer.writeStartElement(STRUCTURE_TAG_NAME);
             version.getStructure().getAttributes().forEach(attribute -> {
                 try {
                     writer.writeStartElement("row");
@@ -177,7 +197,7 @@ public class XmlFileGenerator extends PerRowFileGenerator {
 
     private void addAttributeValidation(String attributeCode) {
 
-        if(attributeValidations == null)
+        if (attributeValidations == null)
             return;
 
         attributeValidations.stream()
@@ -201,13 +221,15 @@ public class XmlFileGenerator extends PerRowFileGenerator {
     }
 
     private void writeElement(String elementName, String body) {
-        if(body == null) {
+
+        if (body == null)
             return;
-        }
+
         try {
             writer.writeStartElement(elementName);
             writer.writeCharacters(body);
             writer.writeEndElement();
+
         } catch (XMLStreamException e) {
             throwXmlGenerateError(e);
         }
@@ -223,6 +245,7 @@ public class XmlFileGenerator extends PerRowFileGenerator {
     public void close() {
         try {
             writer.close();
+
         } catch (XMLStreamException e) {
             logger.error("cannot close output", e);
         }
