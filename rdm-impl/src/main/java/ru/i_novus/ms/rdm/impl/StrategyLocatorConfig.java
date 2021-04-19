3154a655d64d4620e4c3a7e0c43c7f6ded45545c
arahmatullin <arahmatullin@i-novus.ru>
2021-04-19 14:47:21 +0300

    nf RDM-828 Добавление стратегий валидации черновика в конфиг локатора.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/StrategyLocatorConfig.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/StrategyLocatorConfig.java
@@ -7,6 +7,7 @@ import ru.i_novus.ms.rdm.api.model.refbook.RefBookType;
 import ru.i_novus.ms.rdm.impl.strategy.BaseStrategyLocator;
 import ru.i_novus.ms.rdm.impl.strategy.Strategy;
 import ru.i_novus.ms.rdm.impl.strategy.StrategyLocator;
+import ru.i_novus.ms.rdm.impl.strategy.draft.*;
 import ru.i_novus.ms.rdm.impl.strategy.refbook.*;
 
 import java.util.EnumMap;
@@ -29,10 +30,19 @@ public class StrategyLocatorConfig {
     private DefaultCreateFirstStorageStrategy defaultCreateFirstStorageStrategy;
 
     @Autowired
+    private DefaultValidateDraftExistsStrategy defaultValidateDraftExistsStrategy;
+
+    @Autowired
+    private DefaultValidateDraftNotArchivedStrategy defaultValidateDraftNotArchivedStrategy;
+
+    @Autowired
     private UnversionedCreateFirstVersionStrategy unversionedCreateFirstVersionStrategy;
 
     @Autowired
     private UnversionedCreateFirstStorageStrategy unversionedCreateFirstStorageStrategy;
+
+    @Autowired
+    private UnversionedValidateDraftExistsStrategy unversionedValidateDraftExistsStrategy;
 
     @Bean
     public StrategyLocator strategyLocator() {
@@ -51,10 +61,14 @@ public class StrategyLocatorConfig {
     private Map<Class<? extends Strategy>, Strategy> getDefaultStrategies() {
 
         Map<Class<? extends Strategy>, Strategy> result = new HashMap<>();
+        // RefBook:
         result.put(RefBookCreateValidationStrategy.class, defaultRefBookCreateValidationStrategy);
         result.put(CreateRefBookEntityStrategy.class, defaultCreateRefBookEntityStrategy);
         result.put(CreateFirstVersionStrategy.class, defaultCreateFirstVersionStrategy);
         result.put(CreateFirstStorageStrategy.class, defaultCreateFirstStorageStrategy);
+        // Draft:
+        result.put(ValidateDraftExistsStrategy.class, defaultValidateDraftExistsStrategy);
+        result.put(ValidateDraftNotArchivedStrategy.class, defaultValidateDraftNotArchivedStrategy);
 
         return result;
     }
@@ -62,8 +76,11 @@ public class StrategyLocatorConfig {
     private Map<Class<? extends Strategy>, Strategy> getUnversionedStrategies() {
 
         Map<Class<? extends Strategy>, Strategy> result = new HashMap<>();
+        // RefBook:
         result.put(CreateFirstVersionStrategy.class, unversionedCreateFirstVersionStrategy);
         result.put(CreateFirstStorageStrategy.class, unversionedCreateFirstStorageStrategy);
+        // Draft:
+        result.put(ValidateDraftExistsStrategy.class, unversionedValidateDraftExistsStrategy);
 
         return result;
     }
