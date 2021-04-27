40fb53c4eec3fd9307534f36fe3a0486cd5e2b72
arahmatullin <arahmatullin@i-novus.ru>
2021-04-27 11:55:15 +0300

    nf RDM-847 Тест `BaseUiStrategyLocator`.

---
+++ rdm-n2o/src/test/java/ru/i_novus/ms/rdm/n2o/strategy/BaseUiStrategyLocatorTest.java
@@ -0,0 +1,108 @@
+package ru.i_novus.ms.rdm.n2o.strategy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.i_novus.ms.rdm.api.model.refbook.RefBookType;
+import ru.i_novus.ms.rdm.n2o.strategy.draft.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BaseUiStrategyLocatorTest {
+
+    @Mock
+    private DefaultFindOrCreateDraftStrategy defaultFindOrCreateDraftStrategy;
+    @Mock
+    private UnversionedFindOrCreateDraftStrategy unversionedFindOrCreateDraftStrategy;
+
+    @Mock
+    private DefaultValidateIsDraftStrategy defaultValidateIsDraftStrategy;
+
+    /**
+     * Случай:
+     * Стратегия для UNVERSIONED задана и не совпадает со стратегией для DEFAULT.
+     */
+    @Test
+    public void testGetStrategyWhenBothPresent() {
+
+        BaseUiStrategyLocator locator = new BaseUiStrategyLocator(getStrategies());
+
+        FindOrCreateDraftStrategy defaultStrategy =
+                locator.getStrategy(RefBookType.DEFAULT, FindOrCreateDraftStrategy.class);
+        FindOrCreateDraftStrategy unversionedStrategy =
+                locator.getStrategy(RefBookType.UNVERSIONED, FindOrCreateDraftStrategy.class);
+
+        assertNotNull(defaultStrategy);
+        assertNotNull(unversionedStrategy);
+        assertNotEquals(defaultStrategy, unversionedStrategy);
+    }
+
+    /**
+     * Случай:
+     * Стратегия для UNVERSIONED не задана, используется стратегия для DEFAULT.
+     */
+    @Test
+    public void testGetStrategyWhenDefaultOnly() {
+
+        BaseUiStrategyLocator locator = new BaseUiStrategyLocator(getStrategies());
+
+        ValidateIsDraftStrategy defaultStrategy =
+                locator.getStrategy(RefBookType.DEFAULT, ValidateIsDraftStrategy.class);
+        ValidateIsDraftStrategy unversionedStrategy =
+                locator.getStrategy(RefBookType.UNVERSIONED, ValidateIsDraftStrategy.class);
+
+        assertNotNull(defaultStrategy);
+        assertNotNull(unversionedStrategy);
+        assertEquals(defaultStrategy, unversionedStrategy);
+    }
+
+    /**
+     * Случай:
+     * Стратегии для DEFAULT и для UNVERSIONED не заданы.
+     */
+    @Test
+    public void testGetStrategyWhenBothAbsent() {
+
+        // Т.к. нет 3-й интерфейса стратегии, вместо getStrategies() заглушка:
+        BaseUiStrategyLocator locator = new BaseUiStrategyLocator(new HashMap<>());
+
+        FindOrCreateDraftStrategy defaultStrategy =
+                locator.getStrategy(RefBookType.DEFAULT, FindOrCreateDraftStrategy.class);
+        FindOrCreateDraftStrategy unversionedStrategy =
+                locator.getStrategy(RefBookType.UNVERSIONED, FindOrCreateDraftStrategy.class);
+
+        assertNull(defaultStrategy);
+        assertNull(unversionedStrategy);
+    }
+
+    private Map<RefBookType, Map<Class<? extends UiStrategy>, UiStrategy>> getStrategies() {
+
+        Map<RefBookType, Map<Class<? extends UiStrategy>, UiStrategy>> result = new HashMap<>();
+        result.put(RefBookType.DEFAULT, getDefaultStrategies());
+        result.put(RefBookType.UNVERSIONED, getUnversionedStrategies());
+
+        return result;
+    }
+
+    private Map<Class<? extends UiStrategy>, UiStrategy> getDefaultStrategies() {
+
+        Map<Class<? extends UiStrategy>, UiStrategy> result = new HashMap<>();
+        result.put(FindOrCreateDraftStrategy.class, defaultFindOrCreateDraftStrategy);
+        result.put(ValidateIsDraftStrategy.class, defaultValidateIsDraftStrategy);
+
+        return result;
+    }
+
+    private Map<Class<? extends UiStrategy>, UiStrategy> getUnversionedStrategies() {
+
+        Map<Class<? extends UiStrategy>, UiStrategy> result = new HashMap<>();
+        result.put(FindOrCreateDraftStrategy.class, unversionedFindOrCreateDraftStrategy);
+
+        return result;
+    }
+}
