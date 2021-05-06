739c24b213c918ec0247da4bbe075679322ff91c
arahmatullin <arahmatullin@i-novus.ru>
2021-05-06 07:34:43 +0300

    cf RDM-852 Тест стратегий создания хранилища черновика.

---
+++ rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/strategy/draft/DefaultCreateDraftStorageStrategyTest.java
@@ -0,0 +1,37 @@
+package ru.i_novus.ms.rdm.impl.strategy.draft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.i_novus.ms.rdm.api.model.Structure;
+import ru.i_novus.platform.datastorage.temporal.service.DraftDataService;
+
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DefaultCreateDraftStorageStrategyTest {
+
+    @InjectMocks
+    private DefaultCreateDraftStorageStrategy strategy;
+
+    @Mock
+    private DraftDataService draftDataService;
+
+    @Test
+    public void testCreate() {
+
+        final String draftCode = "draft_code";
+        when(draftDataService.createDraft(eq(emptyList()))).thenReturn(draftCode);
+
+        String result = strategy.create(new Structure());
+        assertEquals(draftCode, result);
+
+        verify(draftDataService).createDraft(eq(emptyList()));
+        verifyNoMoreInteractions(draftDataService);
+    }
+}
