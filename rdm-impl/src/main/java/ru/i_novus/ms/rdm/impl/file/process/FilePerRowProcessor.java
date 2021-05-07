8cac56b7691d21f4f7ed0f9d1a007c93000ec203
arahmatullin <arahmatullin@i-novus.ru>
2021-05-07 06:38:31 +0300

    bf RDM-857 Исключение замечаний Sonar.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/file/process/FilePerRowProcessor.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/file/process/FilePerRowProcessor.java
@@ -10,18 +10,20 @@ import ru.i_novus.ms.rdm.impl.util.mappers.RowMapper;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.function.Supplier;
+
+import static java.util.Collections.singletonList;
 
 public abstract class FilePerRowProcessor implements FileProcessor<Result>, Iterator<Row>, Closeable {
 
     private static final Logger logger = LoggerFactory.getLogger(FilePerRowProcessor.class);
 
-    private RowMapper rowMapper;
-    private RowsProcessor rowsProcessor;
+    private final RowMapper rowMapper;
+    private final RowsProcessor rowsProcessor;
 
     public FilePerRowProcessor(RowMapper rowMapper, RowsProcessor rowsProcessor) {
+
         this.rowMapper = rowMapper;
         this.rowsProcessor = rowsProcessor;
     }
@@ -29,6 +31,7 @@ public abstract class FilePerRowProcessor implements FileProcessor<Result>, Iter
     protected abstract void setFile(InputStream inputStream);
 
     public Result process(Supplier<InputStream> fileSource) {
+
         try (InputStream inputStream = fileSource.get()) {
             setFile(inputStream);
 
@@ -38,7 +41,7 @@ public abstract class FilePerRowProcessor implements FileProcessor<Result>, Iter
 
         } catch (IOException e) {
             logger.error("cannot get InputStream", e);
-            return new Result(0, 0, Collections.singletonList(new Message("io.error", e.getMessage())));
+            return new Result(0, 0, singletonList(new Message("io.error", e.getMessage())));
         }
 
         return rowsProcessor.process();
