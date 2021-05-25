2b05b5797bf7cf5f3e7fae08b1bd802b0410d0ab
arahmatullin <arahmatullin@i-novus.ru>
2021-05-25 14:42:49 +0300

    cf RDM-879 Рефакторинг работы с `RowDiff`.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/util/RowDiffUtils.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/util/RowDiffUtils.java
@@ -3,30 +3,32 @@ package ru.i_novus.ms.rdm.impl.util;
 import ru.i_novus.platform.datastorage.temporal.model.FieldValue;
 import ru.i_novus.platform.datastorage.temporal.model.value.RowValue;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@SuppressWarnings({"rawtypes","java:S3740"})
 public class RowDiffUtils {
 
     private RowDiffUtils() {
     }
 
     public static RowDiff getRowDiff(RowValue oldRowValue, RowValue newRowValue) {
+
         RowDiff rowDiff = new RowDiff();
         @SuppressWarnings("unchecked")
         List<FieldValue> fieldValues = oldRowValue.getFieldValues();
         for (FieldValue fieldValue : fieldValues) {
             RowDiff.CellDiff<Object> cellDiff = getCellDiff(fieldValue, newRowValue);
-            if (cellDiff != null)
-                rowDiff.addDiff(fieldValue.getField(), cellDiff);
+            rowDiff.addDiff(fieldValue.getField(), cellDiff);
         }
         return rowDiff;
     }
 
+    @NotNull
     private static RowDiff.CellDiff<Object> getCellDiff(FieldValue oldFieldValue, RowValue newRowValue) {
+
         Object oldValue = oldFieldValue.getValue();
         FieldValue newValue = newRowValue.getFieldValue(oldFieldValue.getField());
-        if (newValue == null)
-            return RowDiff.CellDiff.of(oldValue, null);
-        return RowDiff.CellDiff.of(oldValue, newValue.getValue());
+        return RowDiff.CellDiff.of(oldValue, newValue != null ? newValue.getValue() : null);
     }
 }
