bca45256cb62e7381c071bf2c6417e10196db06b
arahmatullin <arahmatullin@i-novus.ru>
2021-06-02 10:27:09 +0300

    cf RDM-879 Вынесение общих методов теста в `UnversionedBaseRowValuesStrategyTest`.

--- rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/strategy/data/UnversionedDeleteRowValuesStrategyTest.java
+++ rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/strategy/data/UnversionedDeleteRowValuesStrategyTest.java
@@ -6,29 +6,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import ru.i_novus.ms.rdm.api.enumeration.RefBookSourceType;
-import ru.i_novus.ms.rdm.api.enumeration.RefBookStatusType;
-import ru.i_novus.ms.rdm.api.enumeration.RefBookVersionStatus;
-import ru.i_novus.ms.rdm.api.model.Structure;
-import ru.i_novus.ms.rdm.api.model.refdata.RefBookRowValue;
-import ru.i_novus.ms.rdm.api.model.version.ReferrerVersionCriteria;
-import ru.i_novus.ms.rdm.impl.entity.DefaultRefBookEntity;
-import ru.i_novus.ms.rdm.impl.entity.RefBookEntity;
 import ru.i_novus.ms.rdm.impl.entity.RefBookVersionEntity;
 import ru.i_novus.ms.rdm.impl.repository.RefBookConflictRepository;
 import ru.i_novus.ms.rdm.impl.repository.RefBookVersionRepository;
-import ru.i_novus.platform.datastorage.temporal.enums.FieldType;
-import ru.i_novus.platform.datastorage.temporal.model.DisplayExpression;
-import ru.i_novus.platform.datastorage.temporal.model.Reference;
-import ru.i_novus.platform.datastorage.temporal.model.value.IntegerFieldValue;
-import ru.i_novus.platform.datastorage.temporal.model.value.ReferenceFieldValue;
+import ru.i_novus.platform.datastorage.temporal.model.LongRowValue;
 import ru.i_novus.platform.datastorage.temporal.model.value.RowValue;
 import ru.i_novus.platform.datastorage.temporal.service.DraftDataService;
 import ru.i_novus.platform.datastorage.temporal.service.SearchDataService;
 
-import java.math.BigInteger;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
@@ -36,34 +21,21 @@ import static java.util.Collections.singletonList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static ru.i_novus.ms.rdm.api.util.RowUtils.toLongSystemIds;
-import static ru.i_novus.ms.rdm.impl.util.StructureTestConstants.ID_ATTRIBUTE_CODE;
-import static ru.i_novus.ms.rdm.impl.util.StructureTestConstants.NAME_ATTRIBUTE_CODE;
 
 @RunWith(MockitoJUnitRunner.class)
-public class UnversionedDeleteRowValuesStrategyTest extends BaseRowValuesStrategyTest {
-
-    private static final Long DRAFT_SYSTEM_ID = 22L;
-    private static final BigInteger DRAFT_PRIMARY_ID = BigInteger.valueOf(2222L);
-
-    private static final int REFERRER_ID = 10;
-    private static final String REFERRER_CODE = "refer";
-
-    private static final int REFERRER_VERSION_ID = 33;
-    protected static final String REFERRER_VERSION_CODE = "refer_code";
-    private static final String REFERRER_ATTRIBUTE_CODE = "ref";
-    private static final Long REFERRER_SYSTEM_ID = 333L;
+public class UnversionedDeleteRowValuesStrategyTest extends UnversionedBaseRowValuesStrategyTest {
 
     @InjectMocks
     private UnversionedDeleteRowValuesStrategy strategy;
+
+    @Mock
+    private RefBookVersionRepository versionRepository;
 
     @Mock
     private RefBookConflictRepository conflictRepository;
 
     @Mock
     private DraftDataService draftDataService;
-
-    @Mock
-    private RefBookVersionRepository versionRepository;
 
     @Mock
     private SearchDataService searchDataService;
@@ -73,34 +45,27 @@ public class UnversionedDeleteRowValuesStrategyTest extends BaseRowValuesStrateg
 
         RefBookVersionEntity entity = createDraftEntity();
 
+        Integer referredId = 3;
         List<Object> systemIds = List.of(1L, 2L);
 
-        RefBookRowValue entityRowValue = createEntityRowValue();
+        LongRowValue rowValue = createRowValue(1L, referredId);
 
-        CollectionPage<RowValue> entityPagedData = new CollectionPage<>();
-        entityPagedData.init(1, List.of(entityRowValue));
+        CollectionPage<RowValue> pagedData = new CollectionPage<>();
+        pagedData.init(1, List.of(rowValue));
 
         // .before
         RefBookVersionEntity referrer = createReferrerVersionEntity();
         List<RefBookVersionEntity> referrers = singletonList(referrer);
+        mockFindReferrers(versionRepository, referrers);
 
-        final RefBookStatusType referrerStatus = RefBookStatusType.USED;
-        final RefBookSourceType referrerSource = RefBookSourceType.ALL;
-        ReferrerVersionCriteria referrerCriteria = new ReferrerVersionCriteria(REFBOOK_CODE, referrerStatus, referrerSource);
-
-        when(versionRepository.findReferrerVersions(eq(REFBOOK_CODE),
-                eq(referrerStatus.name()), eq(referrerSource.name()), any(Pageable.class))
-        ).thenReturn(new PageImpl<>(referrers, referrerCriteria, 1)) // referrers
-                .thenReturn(new PageImpl<>(emptyList(), referrerCriteria, 1)); // stop
-
-        RefBookRowValue referrerRowValue = createReferrerRowValue();
-
-        CollectionPage<RowValue> referrerPagedData = new CollectionPage<>();
-        referrerPagedData.init(1, List.of(referrerRowValue));
+        Long refSystemId = referredId * REFERRER_SYSTEM_ID_MULTIPLIER;
+        LongRowValue refRowValue = createReferrerRowValue(refSystemId, referredId);
+        CollectionPage<RowValue> refPagedData = new CollectionPage<>();
+        refPagedData.init(1, List.of(refRowValue));
 
         when(searchDataService.getPagedData(any()))
-                .thenReturn(entityPagedData) // page with entity data
-                .thenReturn(referrerPagedData) // page with referrer data
+                .thenReturn(pagedData) // page with entity data
+                .thenReturn(refPagedData) // page with referrer data
                 .thenReturn(new CollectionPage<>(1, emptyList(), null)); // stop
 
         strategy.delete(entity, systemIds);
@@ -113,74 +78,15 @@ public class UnversionedDeleteRowValuesStrategyTest extends BaseRowValuesStrateg
                 .deleteByReferrerVersionIdAndRefRecordIdIn(eq(entity.getId()), eq(toLongSystemIds(systemIds)));
 
         // .before
-        verify(versionRepository, times(2)).findReferrerVersions(eq(REFBOOK_CODE),
-                        eq(referrerStatus.name()), eq(referrerSource.name()), any(Pageable.class));
+        verifyFindReferrers(versionRepository);
 
         verify(conflictRepository)
-                .deleteByReferrerVersionIdAndRefRecordIdIn(eq(referrer.getId()), eq(singletonList(REFERRER_SYSTEM_ID)));
+                .deleteByReferrerVersionIdAndRefRecordIdIn(eq(referrer.getId()), eq(singletonList(refSystemId)));
 
         verify(searchDataService, times(3)).getPagedData(any());
 
         verify(conflictRepository).saveAll(anyList());
 
-        verifyNoMoreInteractions(conflictRepository, draftDataService, versionRepository, searchDataService);
-    }
-
-    private RefBookVersionEntity createReferrerVersionEntity() {
-
-        RefBookVersionEntity entity = new RefBookVersionEntity();
-        entity.setId(REFERRER_VERSION_ID);
-        entity.setRefBook(createReferrerEntity());
-        entity.setStructure(createReferrerStructure());
-        entity.setStorageCode(REFERRER_VERSION_CODE);
-        entity.setStatus(RefBookVersionStatus.PUBLISHED);
-
-        return entity;
-    }
-
-    private RefBookEntity createReferrerEntity() {
-
-        RefBookEntity entity = new DefaultRefBookEntity();
-        entity.setId(REFERRER_ID);
-        entity.setCode(REFERRER_CODE);
-
-        return entity;
-    }
-
-    private Structure createReferrerStructure() {
-
-        Structure.Attribute refAttribute = Structure.Attribute.build(REFERRER_ATTRIBUTE_CODE, REFERRER_ATTRIBUTE_CODE, FieldType.REFERENCE, null);
-        Structure.Reference refReference = new Structure.Reference(REFERRER_ATTRIBUTE_CODE, REFBOOK_CODE, DisplayExpression.toPlaceholder(NAME_ATTRIBUTE_CODE));
-
-        Structure structure = new Structure();
-        structure.add(refAttribute, refReference);
-
-        return structure;
-    }
-
-    private RefBookRowValue createEntityRowValue() {
-
-        RefBookRowValue rowValue = new RefBookRowValue();
-        rowValue.setSystemId(DRAFT_SYSTEM_ID);
-
-        rowValue.setFieldValues(singletonList(
-                new IntegerFieldValue(ID_ATTRIBUTE_CODE, DRAFT_PRIMARY_ID)
-        ));
-
-        return rowValue;
-    }
-
-    private RefBookRowValue createReferrerRowValue() {
-
-        RefBookRowValue rowValue = new RefBookRowValue();
-        rowValue.setSystemId(REFERRER_SYSTEM_ID);
-
-        Reference reference = new Reference(DRAFT_PRIMARY_ID.toString(), "one");
-
-        rowValue.setFieldValues(singletonList(
-                new ReferenceFieldValue(REFERRER_ATTRIBUTE_CODE, reference)
-        ));
-
-        return rowValue;
+        verifyNoMoreInteractions(versionRepository, conflictRepository, draftDataService, searchDataService);
     }
 }
