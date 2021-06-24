ae5cc0eb5765fd3b012f8e9c621d7a196470b321
arahmatullin <arahmatullin@i-novus.ru>
2021-06-24 11:50:40 +0300

    cf RDM-892 Увеличение таймаута.

--- ui-test/src/test/java/ru/inovus/ms/rdm/ui/test/RdmUiTest.java
+++ ui-test/src/test/java/ru/inovus/ms/rdm/ui/test/RdmUiTest.java
@@ -69,7 +69,7 @@ public class RdmUiTest {
     public static void setUp() {
 
         Configuration.baseUrl = getBaseUrl();
-        Configuration.timeout = 8000;
+        Configuration.timeout = 12000;
     }
 
     // URL запущенного фронтенда RDM.
