118fb3cc9406e44d900dee50b4f817b297ff2b9f
arahmatullin <arahmatullin@i-novus.ru>
2021-06-29 09:03:06 +0300

    cf RDM-895 Тест валидации версии (конец).

--- rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/validation/VersionValidationTest.java
+++ rdm-impl/src/test/java/ru/i_novus/ms/rdm/impl/validation/VersionValidationTest.java
@@ -1,6 +1,5 @@
 package ru.i_novus.ms.rdm.impl.validation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import net.n2oapp.platform.i18n.UserException;
 import org.junit.Test;
@@ -8,24 +7,29 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import ru.i_novus.ms.rdm.api.enumeration.RefBookSourceType;
+import ru.i_novus.ms.rdm.api.enumeration.RefBookStatusType;
 import ru.i_novus.ms.rdm.api.exception.NotFoundException;
+import ru.i_novus.ms.rdm.api.model.Structure;
 import ru.i_novus.ms.rdm.impl.BaseTest;
 import ru.i_novus.ms.rdm.impl.repository.RefBookRepository;
 import ru.i_novus.ms.rdm.impl.repository.RefBookVersionRepository;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static ru.i_novus.ms.rdm.impl.util.StructureTestConstants.*;
 
 @RunWith(MockitoJUnitRunner.class)
-@SuppressWarnings("java:S5778")
 public class VersionValidationTest extends BaseTest {
 
     private static final Integer REFBOOK_ID = -10;
     private static final String REFBOOK_CODE = "test";
     private static final Integer VERSION_ID = 2;
-
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Integer DRAFT_ID = 6;
+    private static final Structure STRUCTURE = new Structure(DEFAULT_STRUCTURE);
 
     @InjectMocks
     private VersionValidationImpl versionValidation;
@@ -41,99 +45,210 @@ public class VersionValidationTest extends BaseTest {
 
         when(refBookRepository.existsById(REFBOOK_ID)).thenReturn(true);
         when(versionRepository.exists(any(BooleanExpression.class))).thenReturn(false);
-        try {
-            versionValidation.validateRefBook(REFBOOK_ID);
+        validateSuccess(
+                () -> versionValidation.validateRefBook(REFBOOK_ID)
+        );
+    }
 
-        } catch (RuntimeException e) {
-            fail(e.getMessage());
-        }
+    @Test
+    public void testValidateRefBookFail() {
+
+        when(refBookRepository.existsById(REFBOOK_ID)).thenReturn(true);
+        when(versionRepository.exists(any(BooleanExpression.class))).thenReturn(true);
+        validateFailure(
+                () -> versionValidation.validateRefBook(REFBOOK_ID),
+                UserException.class,
+                "refbook.is.archived"
+        );
     }
 
     @Test
     public void testValidateRefBookCode() {
-        try {
-            versionValidation.validateRefBookCode("aBook-0");
 
-        } catch (RuntimeException e) {
-            fail(e.getMessage());
-        }
+        validateSuccess(
+                () -> versionValidation.validateRefBookCode("aBook-0")
+        );
     }
 
     @Test
     public void testValidateRefBookCodeFail() {
-        try {
-            versionValidation.validateRefBookCode("Реестр");
-            fail(getFailedMessage(UserException.class));
 
-        } catch (UserException e) {
-            assertNotNull(e.getMessages());
-            assertEquals(2, e.getMessages().size());
-            assertEquals("refbook.code.is.invalid", e.getMessages().get(0).getCode());
-            assertEquals("code.is.invalid", e.getMessages().get(1).getCode());
-        }
+        testValidateRefBookCodeFail(null);
+        testValidateRefBookCodeFail("Реестр");
+        testValidateRefBookCodeFail("001");
+        testValidateRefBookCodeFail("Book/2");
+    }
+
+    private void testValidateRefBookCodeFail(String refBookCode) {
+
+        validateFailure(
+                () -> versionValidation.validateRefBookCode(refBookCode),
+                UserException.class,
+                List.of("refbook.code.is.invalid", "code.is.invalid")
+        );
     }
 
     @Test
     public void testValidateRefBookExists() {
 
         when(refBookRepository.existsById(REFBOOK_ID)).thenReturn(true);
-        try {
-            versionValidation.validateRefBookExists(REFBOOK_ID);
+        validateSuccess(
+                () -> versionValidation.validateRefBookExists(REFBOOK_ID)
+        );
+    }
 
-        } catch (RuntimeException e) {
-            fail(e.getMessage());
-        }
+    @Test
+    public void testValidateRefBookExistsFail() {
+
+        when(refBookRepository.existsById(REFBOOK_ID)).thenReturn(false);
+        validateFailure(
+                () -> versionValidation.validateRefBookExists(REFBOOK_ID),
+                NotFoundException.class,
+                "refbook.not.found"
+        );
     }
 
     @Test
     public void testValidateRefBookCodeExists() {
 
         when(refBookRepository.existsByCode(REFBOOK_CODE)).thenReturn(true);
-        try {
-            versionValidation.validateRefBookCodeExists(REFBOOK_CODE);
-
-        } catch (RuntimeException e) {
-            fail(e.getMessage());
-        }
+        validateSuccess(
+                () -> versionValidation.validateRefBookCodeExists(REFBOOK_CODE)
+        );
     }
 
     @Test
     public void testValidateRefBookCodeExistsFail() {
 
         when(refBookRepository.existsByCode(REFBOOK_CODE)).thenReturn(false);
-        try {
-            versionValidation.validateRefBookCodeExists(REFBOOK_CODE);
-            fail(getFailedMessage(NotFoundException.class));
-
-        } catch (RuntimeException e) {
-            assertEquals(NotFoundException.class, e.getClass());
-            assertEquals("refbook.with.code.not.found", getExceptionMessage(e));
-        }
+        validateFailure(
+                () -> versionValidation.validateRefBookCodeExists(REFBOOK_CODE),
+                NotFoundException.class,
+                "refbook.with.code.not.found"
+        );
     }
 
     @Test
     public void testValidateRefBookCodeNotExists() {
 
         when(refBookRepository.existsByCode(REFBOOK_CODE)).thenReturn(false);
-        try {
-            versionValidation.validateRefBookCodeNotExists(REFBOOK_CODE);
-
-        } catch (RuntimeException e) {
-            fail(e.getMessage());
-        }
+        validateSuccess(
+                () -> versionValidation.validateRefBookCodeNotExists(REFBOOK_CODE)
+        );
     }
 
     @Test
     public void testValidateRefBookCodeNotExistsFail() {
 
         when(refBookRepository.existsByCode(REFBOOK_CODE)).thenReturn(true);
-        try {
-            versionValidation.validateRefBookCodeNotExists(REFBOOK_CODE);
-            fail(getFailedMessage(UserException.class));
+        validateFailure(
+                () -> versionValidation.validateRefBookCodeNotExists(REFBOOK_CODE),
+                UserException.class,
+                "refbook.with.code.already.exists"
+        );
+    }
 
-        } catch (RuntimeException e) {
-            assertEquals(UserException.class, e.getClass());
-            assertEquals("refbook.with.code.already.exists", getExceptionMessage(e));
-        }
+    @Test
+    public void testHasReferrerVersions() {
+
+        when(versionRepository.existsReferrerVersions(REFBOOK_CODE, RefBookStatusType.ALL.name(), RefBookSourceType.ALL.name()))
+                .thenReturn(true);
+
+        boolean hasReferrers = versionValidation.hasReferrerVersions(REFBOOK_CODE);
+        assertTrue(hasReferrers);
+    }
+
+    @Test
+    public void testValidateVersionExists() {
+
+        when(versionRepository.existsById(VERSION_ID)).thenReturn(true);
+        validateSuccess(
+                () -> versionValidation.validateVersionExists(VERSION_ID)
+        );
+    }
+
+    @Test
+    public void testValidateVersionExistsFail() {
+
+        when(versionRepository.existsById(VERSION_ID)).thenReturn(false);
+        validateFailure(
+                () -> versionValidation.validateVersionExists(VERSION_ID),
+                NotFoundException.class,
+                "version.not.found"
+        );
+    }
+
+    @Test
+    public void testValidateOptLockValue() {
+
+        validateSuccess(
+                () -> versionValidation.validateOptLockValue(DRAFT_ID, 3, 3)
+        );
+    }
+
+    @Test
+    public void testValidateOptLockValueFail() {
+
+        validateFailure(
+                () -> versionValidation.validateOptLockValue(DRAFT_ID, 3, 2),
+                UserException.class,
+                "draft.was.changed"
+        );
+    }
+
+    @Test
+    public void testValidateDraftNotArchived() {
+
+        when(versionRepository.exists(any(BooleanExpression.class))).thenReturn(false);
+        validateSuccess(
+                () -> versionValidation.validateDraftNotArchived(DRAFT_ID)
+        );
+    }
+
+    @Test
+    public void testValidateDraftNotArchivedFail() {
+
+        when(versionRepository.exists(any(BooleanExpression.class))).thenReturn(true);
+        validateFailure(
+                () -> versionValidation.validateDraftNotArchived(DRAFT_ID),
+                UserException.class,
+                "refbook.is.archived"
+        );
+    }
+
+    @Test
+    public void testValidateAttributeExists() {
+
+        validateSuccess(
+                () -> versionValidation.validateAttributeExists(VERSION_ID, STRUCTURE, NAME_ATTRIBUTE_CODE)
+        );
+    }
+
+    @Test
+    public void testValidateAttributeExistsFail() {
+
+        validateFailure(
+                () -> versionValidation.validateAttributeExists(VERSION_ID, STRUCTURE, UNKNOWN_ATTRIBUTE_CODE),
+                NotFoundException.class,
+                "version.attribute.not.found"
+        );
+    }
+
+    @Test
+    public void testValidateDraftAttributeExists() {
+
+        validateSuccess(
+                () -> versionValidation.validateDraftAttributeExists(DRAFT_ID, STRUCTURE, NAME_ATTRIBUTE_CODE)
+        );
+    }
+
+    @Test
+    public void testValidateDraftAttributeExistsFail() {
+
+        validateFailure(
+                () -> versionValidation.validateDraftAttributeExists(DRAFT_ID, STRUCTURE, UNKNOWN_ATTRIBUTE_CODE),
+                NotFoundException.class,
+                "draft.attribute.not.found"
+        );
     }
 }
