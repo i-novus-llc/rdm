f27aabfb5281031f5c52c7589d1f67346f2d9ebf
arahmatullin <arahmatullin@i-novus.ru>
2021-07-30 15:19:21 +0300

    nf RDM-910 Добавление `main`.

---
+++ rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/AppConfig.java
@@ -0,0 +1,7 @@
+package ru.i_novus.ms.rdm.sync;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
---
+++ rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/Application.java
@@ -0,0 +1,15 @@
+package ru.i_novus.ms.rdm.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(AppConfig.class)
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
