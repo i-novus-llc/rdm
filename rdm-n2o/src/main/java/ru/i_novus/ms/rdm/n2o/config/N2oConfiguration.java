c8f73ccfdc56125afc60e627df2ec4e7f4a52c82
arahmatullin <arahmatullin@i-novus.ru>
2021-12-01 12:10:20 +0300

    сf RDM-937 Включение конфига n2o в автоконфиг.

--- rdm-n2o/src/main/java/ru/i_novus/ms/rdm/n2o/config/N2oConfiguration.java
+++ rdm-n2o/src/main/java/ru/i_novus/ms/rdm/n2o/config/N2oConfiguration.java
@@ -2,6 +2,7 @@ package ru.i_novus.ms.rdm.n2o.config;
 
 import net.n2oapp.cache.template.SyncCacheTemplate;
 import net.n2oapp.framework.api.MetadataEnvironment;
+import net.n2oapp.framework.api.data.CriteriaConstructor;
 import net.n2oapp.framework.api.data.QueryExceptionHandler;
 import net.n2oapp.framework.api.data.QueryProcessor;
 import net.n2oapp.framework.api.register.MetadataRegister;
@@ -9,7 +10,7 @@ import net.n2oapp.framework.config.compile.pipeline.operation.CompileCacheOperat
 import net.n2oapp.framework.config.compile.pipeline.operation.SourceCacheOperation;
 import net.n2oapp.framework.engine.data.N2oInvocationFactory;
 import net.n2oapp.framework.engine.data.N2oQueryProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,34 +21,39 @@ import ru.i_novus.ms.rdm.n2o.operation.RdmSourceCacheOperation;
 
 import java.util.Collection;
 
+/**
+ * Минимально необходимая конфигурация N2O для RDM.
+ */
 @Configuration
 @SuppressWarnings({"rawtypes","java:S3740"})
 public class N2oConfiguration {
 
-    @Autowired
-    private Collection<CriteriaConstructResolver> criteriaConstructResolvers;
-
     @Bean
-    public RestCriteriaConstructor criteriaConstructor() {
+    @ConditionalOnMissingBean
+    public CriteriaConstructor criteriaConstructor(Collection<CriteriaConstructResolver> criteriaConstructResolvers) {
         return new RestCriteriaConstructor(criteriaConstructResolvers);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public QueryProcessor queryProcessor(N2oInvocationFactory invocationFactory,
                                          QueryExceptionHandler exceptionHandler,
-                                         MetadataEnvironment metadataEnvironment) {
+                                         MetadataEnvironment metadataEnvironment,
+                                         Collection<CriteriaConstructResolver> criteriaConstructResolvers) {
         N2oQueryProcessor queryProcessor = new N2oQueryProcessor(invocationFactory, exceptionHandler);
         queryProcessor.setEnvironment(metadataEnvironment);
-        queryProcessor.setCriteriaResolver(criteriaConstructor());
+        queryProcessor.setCriteriaResolver(criteriaConstructor(criteriaConstructResolvers));
         return queryProcessor;
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public CompileCacheOperation compileCacheOperation(CacheManager cacheManager) {
         return new RdmCompileCacheOperation(new SyncCacheTemplate(cacheManager));
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public SourceCacheOperation sourceCacheOperation(CacheManager cacheManager, MetadataRegister metadataRegister) {
         return new RdmSourceCacheOperation(new SyncCacheTemplate(cacheManager), metadataRegister);
     }
