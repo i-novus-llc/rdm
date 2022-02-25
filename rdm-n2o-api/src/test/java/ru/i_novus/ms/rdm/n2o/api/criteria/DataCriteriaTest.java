a1fc02e6c90307c192a314459259c4a8515d1ef5
arahmatullin <arahmatullin@i-novus.ru>
2022-02-25 09:54:28 +0300

    cf RDM-984 Наследование DataCriteria от DataRecordCriteria.

--- rdm-n2o-api/src/test/java/ru/i_novus/ms/rdm/n2o/api/criteria/DataCriteriaTest.java
+++ rdm-n2o-api/src/test/java/ru/i_novus/ms/rdm/n2o/api/criteria/DataCriteriaTest.java
@@ -1,19 +1,22 @@
 package ru.i_novus.ms.rdm.n2o.api.criteria;
 
-import net.n2oapp.criteria.api.Criteria;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.i_novus.ms.rdm.n2o.api.BaseTest;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataCriteriaTest extends BaseTest {
 
-    private static final int TEST_REFBOOK_VERSION_ID = -10;
-    private static final int TEST_OPT_LOCK_VALUE = 10;
+    private static final int REFBOOK_VERSION_ID = -10;
+    private static final int OPT_LOCK_VALUE = 10;
 
     @Test
     public void testClass() {
 
-        Criteria superCriteria = new Criteria();
+        DataRecordCriteria superCriteria = new DataRecordCriteria();
 
         DataCriteria emptyCriteria = new DataCriteria();
         assertObjects(Assert::assertNotEquals, superCriteria, emptyCriteria);
@@ -38,23 +41,35 @@ public class DataCriteriaTest extends BaseTest {
 
         DataCriteria result = new DataCriteria();
 
-        result.setVersionId(TEST_REFBOOK_VERSION_ID);
-        result.setOptLockValue(TEST_OPT_LOCK_VALUE);
+        result.setVersionId(REFBOOK_VERSION_ID);
+        result.setOptLockValue(OPT_LOCK_VALUE);
         result.setLocaleCode("test");
+        
+        result.setFilter(createFilter());
+        result.setHasDataConflict(Boolean.FALSE);
+
+        return result;
+    }
+
+    private Map<String, Serializable> createFilter() {
+
+        Map<String, Serializable> result = new HashMap<>(2);
+        result.put("field1", "value1");
+        result.put("field2", "value2");
 
         return result;
     }
 
     private DataCriteria cloneCriteria(DataCriteria criteria) {
 
-        DataCriteria result = new DataCriteria(criteria.getVersionId(), criteria.getOptLockValue(),
-                criteria.getFilter(), criteria.getHasDataConflict());
+        DataCriteria result = new DataCriteria(criteria.getPageNumber(), criteria.getPageSize(), criteria.getSort());
 
-        result.setPage(criteria.getPage());
-        result.setSize(criteria.getSize());
-        result.setSortings(criteria.getSortings());
-
+        result.setVersionId(criteria.getVersionId());
+        result.setOptLockValue(criteria.getOptLockValue());
         result.setLocaleCode(criteria.getLocaleCode());
+
+        result.setFilter(criteria.getFilter());
+        result.setHasDataConflict(criteria.getHasDataConflict());
 
         return result;
     }
@@ -63,9 +78,9 @@ public class DataCriteriaTest extends BaseTest {
 
         DataCriteria result = new DataCriteria();
 
-        result.setPage(criteria.getPage());
-        result.setSize(criteria.getSize());
-        result.setSortings(criteria.getSortings());
+        result.setPageNumber(criteria.getPageNumber());
+        result.setPageSize(criteria.getPageSize());
+        result.setOrders(criteria.getOrders());
 
         result.setVersionId(criteria.getVersionId());
         result.setOptLockValue(criteria.getOptLockValue());
