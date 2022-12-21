b70fa07c02246c36bd57dbc91654a88548c5e9bb
xuryrg2034@gmail.com <stalker0234>
2022-12-21 06:48:40 +0300

    revert setupProxy.js

--- rdm-frontend/rdm-react/packages/rdm-frontend/src/setupProxy.js
+++ rdm-frontend/rdm-react/packages/rdm-frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/n2o',
         {
-            target: "http://172.16.1.68:8080/#/",
+            target: "https://cloud.develop.i-novus.ru/admin/#/rdm",
             changeOrigin: true
         }
     ));
