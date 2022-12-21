1b538b02c0e3e59710e7991e21e460dfc32569e1
arahmatullin <arahmatullin@i-novus.ru>
2022-12-21 12:46:52 +0300

    cf RDM-1016 Выделение конфигуратора для маппинга данных.

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/provider/VdsMapperConfigurer.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/provider/VdsMapperConfigurer.java
@@ -1,55 +1,31 @@
 package ru.i_novus.ms.rdm.impl.provider;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import net.n2oapp.platform.jaxrs.MapperConfigurer;
-import org.springframework.data.domain.PageImpl;
-import ru.i_novus.ms.rdm.api.model.diff.DiffFieldValueDeserializer;
-import ru.i_novus.ms.rdm.api.model.diff.DiffRowValueMixin;
-import ru.i_novus.ms.rdm.api.model.field.FieldValueMixin;
-import ru.i_novus.ms.rdm.api.model.refdata.RowValueMixin;
-import ru.i_novus.ms.rdm.impl.model.field.VdsFieldMixin;
-import ru.i_novus.platform.datastorage.temporal.model.Field;
-import ru.i_novus.platform.datastorage.temporal.model.FieldValue;
-import ru.i_novus.platform.datastorage.temporal.model.value.DiffFieldValue;
-import ru.i_novus.platform.datastorage.temporal.model.value.DiffRowValue;
-import ru.i_novus.platform.datastorage.temporal.model.value.RowValue;
+import ru.i_novus.ms.rdm.api.provider.DataMapperConfigurer;
 
-@SuppressWarnings("rawtypes")
-public class VdsMapperConfigurer implements MapperConfigurer {
+public class VdsMapperConfigurer extends DataMapperConfigurer implements MapperConfigurer {
 
     @Override
     public void configure(ObjectMapper mapper) {
 
-        mapper.addMixIn(Field.class, VdsFieldMixin.class);
-        mapper.addMixIn(FieldValue.class, FieldValueMixin.class);
-        mapper.addMixIn(RowValue.class, RowValueMixin.class);
-        mapper.writerFor(new TypeReference<PageImpl<RowValue>>() {});
-
-        mapper.addMixIn(DiffRowValue.class, DiffRowValueMixin.class);
-
-        SimpleModule module = new SimpleModule()
-                .addDeserializer(DiffFieldValue.class, new DiffFieldValueDeserializer());
-        mapper.registerModule(module);
+        super.configure(mapper);
 
         mapper.enable(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS);
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         // (Де)сериализация даты/времени:
-        mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // в строку / из строки
         mapper.disable(SerializationFeature.WRITE_DATES_WITH_ZONE_ID); // без временно'й зоны
 
         // (Де)сериализация значений, хранящихся в полях класса Object:
-        mapper.activateDefaultTypingAsProperty(
-                BasicPolymorphicTypeValidator.builder().build(),
-                ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT,
-                "@class");
+        //mapper.activateDefaultTypingAsProperty(
+        //        BasicPolymorphicTypeValidator.builder().build(),
+        //        ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT,
+        //        "@class");
 
         // Пропуск значений null:
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
