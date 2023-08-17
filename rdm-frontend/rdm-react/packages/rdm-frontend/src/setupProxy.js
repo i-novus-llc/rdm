6961a70f279c062e08b266d5070e51503be09abe
arahmatullin <arahmatullin@i-novus.ru>
2023-08-17 10:28:11 +0300

    bf RDM-1060 Исправление proxy target.

--- rdm-frontend/rdm-react/packages/rdm-frontend/src/setupProxy.js
+++ rdm-frontend/rdm-react/packages/rdm-frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/n2o',
         {
-            target: "http://docker.one:8180/",
+            target: "https://cloud.develop.i-novus.ru/admin/#/rdm",
             changeOrigin: true
         }
     ));
