4735352e464f185daca258533edff15904b062c9
arahmatullin <arahmatullin@i-novus.ru>
2024-05-03 15:10:10 +0300

    cf EGISZNRPLATFORM-1640 Up versions of n2o-boot and sec-admin for backend
    https://jira.i-novus.ru/browse/EGISZNRPLATFORM-1640

--- rdm-service/src/main/java/ru/i_novus/ms/rdm/service/SecurityConfig.java
+++ rdm-service/src/main/java/ru/i_novus/ms/rdm/service/SecurityConfig.java
@@ -1,14 +1,24 @@
 package ru.i_novus.ms.rdm.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableResourceServer
 public class SecurityConfig {
-//public class SecurityConfig extends N2oPlatformResourceServerConfigurerAdapter {
 
-    //@Override
-    //public void configure(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry requests) {
-    //    requests.antMatchers("/*").permitAll();
-    //}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/*").permitAll()
+                //.anyRequest().authenticated()
+                //.and()
+                //.csrf().disable()
+        ;
+        //http.oauth2ResourceServer(configurer -> configurer.jwt());
+
+        return http.build();
+    }
 }
