b74d5021b4abcc726df5513976d3cf0c2add551e
arahmatullin <arahmatullin@i-novus.ru>
2024-05-29 12:31:49 +0300

    bf EGISZNRPLATFORM-1641 Remove security from rdm-backend
    https://jira.i-novus.ru/browse/EGISZNRPLATFORM-1641

--- rdm-rest/src/main/java/ru/i_novus/ms/rdm/rest/util/SecurityContextUtils.java
+++ rdm-rest/src/main/java/ru/i_novus/ms/rdm/rest/util/SecurityContextUtils.java
@@ -1,10 +1,5 @@
 package ru.i_novus.ms.rdm.rest.util;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 public final class SecurityContextUtils {
 
     private static final String DEFAULT_USER_ID = "UNKNOWN";
@@ -15,35 +10,10 @@ public final class SecurityContextUtils {
     }
 
     public static String getUserId() {
-
         return DEFAULT_USER_ID;
     }
 
     public static String getUserName() {
-
-        final Object principal = getPrincipal();
-        if (principal instanceof String)
-            return (String) principal;
-
         return DEFAULT_USER_NAME;
-    }
-
-    private static Object getPrincipal() {
-
-        final Authentication authentication = getAuthentication();
-        if (authentication == null)
-            return null;
-
-        return authentication.getPrincipal();
-    }
-
-    private static Authentication getAuthentication() {
-
-        final SecurityContext context = SecurityContextHolder.getContext();
-        if (context == null)
-            return null;
-
-        final Authentication authentication = context.getAuthentication();
-        return (authentication instanceof AnonymousAuthenticationToken) ? null : authentication;
     }
 }
