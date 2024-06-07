42e5a691d0f97091050e42e52aa321a8dc475314
xuryrg2034 <xuryrg2034@gmail.com>
2024-06-07 13:53:56 +0300

    story/RDM-1099

--- rdm-frontend/rdm-react/packages/n2o-config-rdm/src/DataGrid/DataGridWidget.jsx
+++ rdm-frontend/rdm-react/packages/n2o-config-rdm/src/DataGrid/DataGridWidget.jsx
@@ -1,44 +1,64 @@
 import React, { useLayoutEffect } from "react";
 import {useDispatch, useSelector} from "react-redux";
-import isEqual from 'lodash/isEqual'
-import { AdvancedTableWidget } from "n2o-framework/lib/components/widgets/AdvancedTable";
+import { compose } from 'recompose'
+
+import { AdvancedTableContainer } from "n2o-framework/lib/components/widgets/AdvancedTable";
+import { WithTableProps } from "n2o-framework/lib/components/widgets/AdvancedTable/WithTableProps";
+import { WidgetHOC } from "n2o-framework/lib/core/widget/WidgetHOC";
+import { withSecurityList } from "n2o-framework/lib/core/auth/withSecurity";
 import { dataSourceModelByPrefixSelector } from "n2o-framework/lib/ducks/datasource/selectors";
 import { ModelPrefix } from "n2o-framework/lib/core/datasource/const";
+import { setModel } from 'n2o-framework/lib/ducks/models/store'
 
 import { getColumnsFromDatasource, getDataFromDatasource } from "./utils";
 import { rdmUpdateConfigField } from "../store";
 
 
 function DataGridWidget(props) {
-  const {
-    table,
-    datasource,
-    id,
-  } = props
+  const { datasource, fetchData } = props
+
   const dispatch = useDispatch()
   const datasourceModel = useSelector(dataSourceModelByPrefixSelector(datasource, ModelPrefix.source))
+  const filterModel = useSelector(dataSourceModelByPrefixSelector(datasource, ModelPrefix.filter))
 
   useLayoutEffect(() => {
     const columns = getColumnsFromDatasource(datasourceModel)
-    const { cells: bodyCells, headers: headerCells } = columns
-    const isEqualBodyCells = isEqual(table.body.cells, bodyCells)
-    const isEqualHeaderCells = isEqual(table.header.cells, headerCells)
+    const data = getDataFromDatasource(datasourceModel)
 
-    if (isEqualBodyCells && isEqualHeaderCells) { return }
+    if (columns) {
+      const { cells: bodyCells, headers: headerCells } = columns
 
-    dispatch(rdmUpdateConfigField(id, 'table.body.cells', bodyCells))
-    dispatch(rdmUpdateConfigField(id, 'table.header.cells', headerCells))
-  }, [datasourceModel, table.body, table.header, id]);
+      dispatch(rdmUpdateConfigField(datasource, 'table.body.cells', bodyCells))
+      dispatch(rdmUpdateConfigField(datasource, 'table.header.cells', headerCells))
+
+      dispatch(setModel(ModelPrefix.source, datasource, data))
+    }
+  }, [datasourceModel]);
+
+  useLayoutEffect(() => {
+    if (filterModel) {
+      const mappedFilter = Object.entries(filterModel)
+        .reduce((out, [key, value]) => {
+          out[`filter.${key}`] = value
+
+          return out;
+        }, {})
+
+      fetchData(mappedFilter)
+    }
+  }, [filterModel, fetchData]);
+
 
   return (
-    <AdvancedTableWidget
+    <AdvancedTableContainer
       {...props}
       className="rdm-data-grid"
-      dataMapper={getDataFromDatasource}
     />
   );
 }
 
 DataGridWidget.displayName = '@rdm/DataGridWidget'
 
-export default DataGridWidget
+export default compose(
+  WidgetHOC,
+)(withSecurityList(WithTableProps(DataGridWidget), 'table.header.cells'))
--- rdm-frontend/rdm-react/packages/n2o-config-rdm/src/DataGrid/utils.js
+++ rdm-frontend/rdm-react/packages/n2o-config-rdm/src/DataGrid/utils.js
@@ -1,16 +1,19 @@
 import get from 'lodash/get'
-import map from 'lodash/map'
-
-const defaultColumns = {
-  cells: [],
-  headers: []
-}
 
 const defaultData = []
 
-export const getColumnsFromDatasource = datasource => get(datasource, [0, 'columnsConfig'], defaultColumns)
+export const getColumnsFromDatasource = datasource => get(datasource, [0, 'columnsConfig'])
 
-export const getDataFromDatasource = datasource => {
-    // console.log('mapModel', map(datasource, item => item.row))
-    return datasource ? map(datasource, item => Object.keys(item.row).length ? item.row : null).filter(Boolean) : defaultData
+export const getDataFromDatasource = model => {
+  if (!model) return defaultData;
+
+  return (
+    model.reduce((out, item) => {
+      if (item.row && Object.keys(item.row).length) {
+        out.push(item.row)
+      }
+
+      return out
+    }, [])
+  )
 }
