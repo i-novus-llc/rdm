ba4b0326bd2f401029ee69bd443445ee877ad4fa
arahmatullin <arahmatullin@i-novus.ru>
2024-06-20 09:35:35 +0300

    bf RDM-1106 Remove userinfo from backend
    https://jira.i-novus.ru/browse/RDM-1106

--- rdm-rest/src/main/java/ru/i_novus/ms/rdm/rest/util/SecurityContextUtils.java
+++ rdm-rest/src/main/java/ru/i_novus/ms/rdm/rest/util/SecurityContextUtils.java
@@ -1,9 +1,5 @@
 package ru.i_novus.ms.rdm.rest.util;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 public final class SecurityContextUtils {
 
     private static final String DEFAULT_USER_ID = "UNKNOWN";
@@ -19,20 +15,5 @@ public final class SecurityContextUtils {
 
     public static String getUserName() {
         return DEFAULT_USER_NAME;
-    }
-
-    public static Object getPrincipal() {
-
-        final Authentication authentication = getAuthentication();
-        if (authentication == null)
-            return null;
-
-        return authentication.getPrincipal();
-    }
-
-    private static Authentication getAuthentication() {
-
-        final SecurityContext context = SecurityContextHolder.getContext();
-        return (context == null) ? null : context.getAuthentication();
     }
 }
