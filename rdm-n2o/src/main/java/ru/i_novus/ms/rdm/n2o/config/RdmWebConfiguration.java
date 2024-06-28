6db331af8ac2ee7e476cbf34c71c0d23ca5c22a2
arahmatullin <arahmatullin@i-novus.ru>
2024-06-28 12:45:15 +0300

    bf RDM-1106 Add userinfo for rest-call
    https://jira.i-novus.ru/browse/RDM-1106

--- rdm-n2o/src/main/java/ru/i_novus/ms/rdm/n2o/config/RdmWebConfiguration.java
+++ rdm-n2o/src/main/java/ru/i_novus/ms/rdm/n2o/config/RdmWebConfiguration.java
@@ -1,16 +1,27 @@
 package ru.i_novus.ms.rdm.n2o.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.n2oapp.framework.api.rest.RestLoggingHandler;
+import net.n2oapp.framework.engine.data.rest.SpringRestDataProviderEngine;
+import net.n2oapp.framework.engine.data.rest.json.RestEngineTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.converter.GenericHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Configuration
 @Import(value = { ClientConfiguration.class, N2oConfiguration.class })
@@ -20,6 +31,18 @@ public class RdmWebConfiguration {
     private final RestTemplate restTemplate;
 
     private final ClientHttpRequestInterceptor userinfoRestTemplateInterceptor;
+
+    @Value("${n2o.engine.rest.url}")
+    private String baseRestUrl;
+
+    @Value("${n2o.engine.rest.dateformat.serialize}")
+    private String serializingFormat;
+
+    @Value("${n2o.engine.rest.dateformat.deserialize}")
+    private String[] deserializingFormats;
+
+    @Value("${n2o.engine.rest.dateformat.exclusion-keys}")
+    private String[] exclusionKeys;
 
     @Autowired
     public RdmWebConfiguration(RestTemplate restTemplate,
@@ -36,7 +59,39 @@ public class RdmWebConfiguration {
             converter instanceof GenericHttpMessageConverter &&
             converter.getSupportedMediaTypes().contains(MediaType.APPLICATION_XML)
         );
+    }
 
+    @Bean
+    public SpringRestDataProviderEngine restDataProviderEngine(RestTemplateBuilder builder, List<RestLoggingHandler> loggingHandlers) {
+        ObjectMapper restObjectMapper = restObjectMapper();
+        SpringRestDataProviderEngine springRestDataProviderEngine = new SpringRestDataProviderEngine(
+                rdmRestTemplate(builder, httpMessageConverter(restObjectMapper)),
+                restObjectMapper, loggingHandlers);
+
+        springRestDataProviderEngine.setBaseRestUrl(baseRestUrl);
+        return springRestDataProviderEngine;
+    }
+
+    private ObjectMapper restObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat(serializingFormat));
+        RestEngineTimeModule module = new RestEngineTimeModule(deserializingFormats, exclusionKeys);
+        objectMapper.registerModules(module);
+        return objectMapper;
+    }
+
+    private MappingJackson2HttpMessageConverter httpMessageConverter(ObjectMapper restObjectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(restObjectMapper);
+        return converter;
+    }
+
+    private RestTemplate rdmRestTemplate(RestTemplateBuilder builder, MappingJackson2HttpMessageConverter converter) {
+        DefaultUriBuilderFactory builderFactory = new DefaultUriBuilderFactory();
+        builderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.TEMPLATE_AND_VALUES);
+        RestTemplate restTemplate = builder.messageConverters(converter).build();
+        restTemplate.setUriTemplateHandler(builderFactory);
         restTemplate.getInterceptors().add(userinfoRestTemplateInterceptor);
+        return restTemplate;
     }
 }
