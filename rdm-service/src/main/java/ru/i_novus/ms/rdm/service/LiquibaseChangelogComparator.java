9868a5e52d42977ae30e901caf76fcbb3b5b0a71
arahmatullin <arahmatullin@i-novus.ru>
2024-08-14 15:02:41 +0300

    cf RDM-1091 Refactor LiquibaseChangelogComparator
    https://jira.i-novus.ru/browse/RDM-1091

--- rdm-service/src/main/java/ru/i_novus/ms/rdm/service/LiquibaseChangelogComparator.java
+++ rdm-service/src/main/java/ru/i_novus/ms/rdm/service/LiquibaseChangelogComparator.java
@@ -3,16 +3,22 @@ package ru.i_novus.ms.rdm.service;
 import java.util.Comparator;
 
 /**
- * modified from liquibase.changelog.DatabaseChangeLog#getStandardChangeLogComparator()
+ * Comparator to order script files by filename only.
+ * <p>
+ * Based on {@link liquibase.changelog.DatabaseChangeLog#getStandardChangeLogComparator() getStandardChangeLogComparator}
  */
 public class LiquibaseChangelogComparator implements Comparator<String> {
+
     @Override
     public int compare(String o1, String o2) {
+
         // by ignoring WEB-INF/classes in path all changelog Files independent
         // whether they are in a WAR or in a JAR are order following the same rule
-        return o1.replaceFirst("^\\/", "").replace("WEB-INF/classes/", "")
-                .compareTo(
-                        o2.replaceFirst("^\\/", "").replace("WEB-INF/classes/", "")
-                );
+        return prepareId(o1).compareTo(prepareId(o2));
+    }
+
+    private static String prepareId(String id) {
+        return id.replaceFirst("^/", "")
+                .replace("WEB-INF/classes/", "");
     }
 }
