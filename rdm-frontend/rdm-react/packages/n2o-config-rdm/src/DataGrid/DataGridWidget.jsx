be6ccf7ed1a97581bfac00bdaa46eb840c9967c4
xuryrg2034 <xuryrg2034@gmail.com>
2024-08-14 07:32:30 +0300

    story/RDM-1091 - Удален хок withSecurityList из DataGridWidget

--- rdm-frontend/rdm-react/packages/n2o-config-rdm/src/DataGrid/DataGridWidget.jsx
+++ rdm-frontend/rdm-react/packages/n2o-config-rdm/src/DataGrid/DataGridWidget.jsx
@@ -5,7 +5,6 @@ import { compose } from 'recompose'
 import { AdvancedTableContainer } from "n2o-framework/lib/components/widgets/AdvancedTable";
 import { WithTableProps } from "n2o-framework/lib/components/widgets/AdvancedTable/WithTableProps";
 import { WidgetHOC } from "n2o-framework/lib/core/widget/WidgetHOC";
-import { withSecurityList } from "n2o-framework/lib/core/auth/withSecurity";
 import { dataSourceModelByPrefixSelector } from "n2o-framework/lib/ducks/datasource/selectors";
 import { ModelPrefix } from "n2o-framework/lib/core/datasource/const";
 import { setModel } from 'n2o-framework/lib/ducks/models/store'
@@ -15,11 +14,10 @@ import { rdmUpdateConfigField } from "../store";
 
 
 function DataGridWidget(props) {
-  const { datasource, id, fetchData } = props
+  const { datasource, id } = props
 
   const dispatch = useDispatch()
   const datasourceModel = useSelector(dataSourceModelByPrefixSelector(datasource, ModelPrefix.source))
-  const filterModel = useSelector(dataSourceModelByPrefixSelector(datasource, ModelPrefix.filter))
 
   useLayoutEffect(() => {
     const columns = getColumnsFromDatasource(datasourceModel)
@@ -47,4 +45,4 @@ DataGridWidget.displayName = '@rdm/DataGridWidget'
 
 export default compose(
   WidgetHOC,
-)(withSecurityList(WithTableProps(DataGridWidget), 'table.header.cells'))
+)(WithTableProps(DataGridWidget), 'table.header.cells')
