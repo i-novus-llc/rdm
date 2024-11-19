fd50922efcef403aa77fe69d45462d10d09ce38a
arahmatullin <arahmatullin@i-novus.ru>
2024-11-19 11:28:31 +0300

    bf RDM-1154 Исключение генерации count-запроса для native-sql
    https://jira.i-novus.ru/browse/RDM-1154

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/repository/RefBookDetailModelRepository.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/repository/RefBookDetailModelRepository.java
@@ -14,118 +14,115 @@ public interface RefBookDetailModelRepository extends
     /**
      * Проверка наличия справочника, ссылающегося на справочник с кодом refBookCode.
      * <p/>
-     * Аналогичен запросу {@link RefBookVersionRepository#FIND_REFERRER_VERSIONS}.
+     * Аналогичен запросу {@link RefBookVersionRepository#FROM_REFERRER_VERSIONS}.
      */
-    String CHECK_REFERRER_VERSION = "    select 1 \n" +
-            "      from n2o_rdm_management.ref_book_version bv \n" +
-            "     cross join lateral \n" +
-            "           jsonb_to_recordset(bv.\"structure\" -> 'attributes') \n" +
-            "               as akey(\"code\" varchar, \"isPrimary\" bool) \n" +
-            "     cross join lateral \n" +
-            "           jsonb_to_recordset(bv.\"structure\" -> 'attributes') \n" +
-            "               as aref(\"type\" varchar, \"referenceCode\" varchar) \n" +
-            "     where bv.\"structure\" is not null \n" +
-            "       and (bv.\"structure\" -> 'attributes') is not null \n" +
-            "       and akey.\"isPrimary\" = true \n" +
-            "       and aref.\"type\" = 'REFERENCE' \n" +
-            "       and aref.\"referenceCode\" = cv.ref_book_code \n" +
-            "     limit 1 \n";
+    String CHECK_REFERRER_VERSION = """
+            SELECT 1
+              FROM n2o_rdm_management.ref_book_version AS bv
+             CROSS JOIN LATERAL jsonb_to_recordset(bv."structure" -> 'attributes')
+                AS akey("code" varchar, "isPrimary" bool)
+             CROSS JOIN LATERAL jsonb_to_recordset(bv."structure" -> 'attributes')
+                AS aref("type" varchar, "referenceCode" varchar)
+             WHERE bv."structure" is not null
+               AND (bv."structure" -> 'attributes') IS NOT NULL
+               AND akey."isPrimary" = TRUE
+               AND aref."type" = 'REFERENCE'
+               AND aref."referenceCode" = cv.ref_book_code
+             LIMIT 1
+        """;
 
-    String AND_REFERRED_IS_LAST_PUBLISHED = "        and c.published_id = ( \n" +
-            "            select lv.id \n" +
-            "              from n2o_rdm_management.ref_book_version lv \n" +
-            //"             where lv.ref_book_id = pv.ref_book_id \n" +
-            "             where lv.ref_book_id = ( \n" +
-            "                   select pv.ref_book_id \n" +
-            "                     from n2o_rdm_management.ref_book_version pv \n" +
-            "                    where pv.id = c.published_id \n" +
-            "                   ) \n" +
-            "               and lv.status = 'PUBLISHED' \n" +
-            "             order by lv.from_date desc \n" +
-            "             limit 1 ) \n";
+    String AND_REFERRED_IS_LAST_PUBLISHED = """
+               AND c.published_id = (
+                   SELECT lv.id
+                     FROM n2o_rdm_management.ref_book_version AS lv
+                    WHERE lv.ref_book_id = (
+                          SELECT pv.ref_book_id
+                            FROM n2o_rdm_management.ref_book_version AS pv
+                           WHERE pv.id = c.published_id
+                          )
+                      AND lv.status = 'PUBLISHED'
+                    ORDER BY lv.from_date DESC
+                    LIMIT 1
+               )
+        """;
 
-    @SuppressWarnings("squid:S1192")
-    String FIND_BY_VERSION_ID = "with current_version as ( \n" +
-            "  select \n" +
-            "    v.id as id, \n" +
-            "    b.id as ref_book_id, \n" +
-            "    b.code as ref_book_code, \n" +
-            "    b.removable as is_removable, \n" +
-            "    b.archived as is_archived, \n" +
-            "\n" +
+    String WITH_CURRENT_VERSION_BY_ID = """
+            WITH current_version AS (
+                 SELECT
+                   v.id AS id,
+                   b.id AS ref_book_id,
+                   b.code as ref_book_code,
+                   b.removable AS is_removable,
+                   b.archived AS is_archived,
 
-            "    (select lv.id \n" +
-            "       from n2o_rdm_management.ref_book_version lv \n" +
-            "      where lv.ref_book_id = b.id \n" +
-            "        and lv.status = 'DRAFT' \n" +
-            "      limit 1 ) as draft_id, \n" +
-            "\n" +
+                   (SELECT lv.id
+                      FROM n2o_rdm_management.ref_book_version AS lv
+                     WHERE lv.ref_book_id = b.id AND lv.status = 'DRAFT'
+                     LIMIT 1 ) AS draft_id,
+            
+                   (SELECT lv.id
+                      FROM n2o_rdm_management.ref_book_version AS lv
+                     WHERE lv.ref_book_id = b.id AND lv.status = 'PUBLISHED'
+                     ORDER BY lv.from_date DESC
+                     LIMIT 1 ) AS last_published_id
 
-            "    (select lv.id \n" +
-            "       from n2o_rdm_management.ref_book_version lv \n" +
-            "      where lv.ref_book_id = b.id \n" +
-            "        and lv.status = 'PUBLISHED' \n" +
-            "      order by lv.from_date desc \n" +
-            "      limit 1 ) as last_published_id \n" +
-            "\n" +
+                   FROM n2o_rdm_management.ref_book_version AS v
+                  INNER JOIN n2o_rdm_management.ref_book AS b
+                     ON b.id = v.ref_book_id
+                  WHERE v.id = :currentVersionId
+            )
+        """;
 
-            "    from n2o_rdm_management.ref_book_version v \n" +
-            "   inner join n2o_rdm_management.ref_book b \n" +
-            "      on b.id = v.ref_book_id \n" +
-            "   where v.id = :currentVersionId \n" +
-            ") \n" +
+    String SELECT_FROM_CURRENT_VERSION = """
+            SELECT
+              cv.id AS current_version_id,
+              cv.draft_id AS draft_version_id,
+              cv.last_published_id AS last_published_version_id,
 
-            "select \n" +
-            "  cv.id as current_version_id, \n" +
-            "  cv.draft_id as draft_version_id, \n" +
-            "  cv.last_published_id as last_published_version_id, \n" +
+              (cv.is_removable AND NOT cv.is_archived AND
+               cv.last_published_id IS NULL) AS removable,
 
-            "  (cv.is_removable and not cv.is_archived and \n" +
-            "   cv.last_published_id is null) as removable, \n" +
+              EXISTS(
+        """ + CHECK_REFERRER_VERSION + """
+              ) AS has_referrer_version,
 
-            "  exists(\n" + CHECK_REFERRER_VERSION + "  ) as has_referrer_version, \n" +
-            "\n" +
+              EXISTS(
+                  SELECT 1 FROM n2o_rdm_management.ref_book_conflict AS c
+                   WHERE c.referrer_id = cv.id AND c.ref_recordid IS NOT NULL
+        """ + AND_REFERRED_IS_LAST_PUBLISHED + """
+              ) AS has_data_conflict,
 
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = cv.id \n" +
-            "       and c.ref_recordid is not null \n" +
-            AND_REFERRED_IS_LAST_PUBLISHED +
-            "  ) as has_data_conflict, \n" +
+              EXISTS(
+                  SELECT 1 FROM n2o_rdm_management.ref_book_conflict AS c
+                   WHERE c.referrer_id = cv.id AND c.conflict_type = 'UPDATED'
+        """ + AND_REFERRED_IS_LAST_PUBLISHED + """
+              ) AS has_updated_conflict,
 
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = cv.id \n" +
-            "       and c.conflict_type = 'UPDATED' \n" +
-            AND_REFERRED_IS_LAST_PUBLISHED +
-            "  ) as has_updated_conflict, \n" +
+              EXISTS(
+                  SELECT 1 FROM n2o_rdm_management.ref_book_conflict AS c
+                   WHERE c.referrer_id = cv.id AND c.conflict_type = 'ALTERED'
+        """ + AND_REFERRED_IS_LAST_PUBLISHED + """
+              ) AS has_altered_conflict,
 
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = cv.id \n" +
-            "       and c.conflict_type = 'ALTERED' \n" +
-            AND_REFERRED_IS_LAST_PUBLISHED +
-            "  ) as has_altered_conflict, \n" +
+              EXISTS(
+                  SELECT 1 FROM n2o_rdm_management.ref_book_conflict AS c
+                   WHERE c.referrer_id = cv.id AND c.ref_recordid IS NULL
+        """ + AND_REFERRED_IS_LAST_PUBLISHED + """
+              ) AS has_structure_conflict,
 
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = cv.id \n" +
-            "       and c.ref_recordid is null \n" +
-            AND_REFERRED_IS_LAST_PUBLISHED +
-            "  ) as has_structure_conflict, \n" +
+              EXISTS(
+                  SELECT 1 from n2o_rdm_management.ref_book_conflict AS c
+                   WHERE c.referrer_id = cv.last_published_id
+        """ + AND_REFERRED_IS_LAST_PUBLISHED + """
+              ) AS last_has_conflict
 
-            "\n" +
-            "  exists( \n" +
-            "    select 1 from n2o_rdm_management.ref_book_conflict c \n" +
-            "     where c.referrer_id = cv.last_published_id" +
-            AND_REFERRED_IS_LAST_PUBLISHED +
-            "  ) as last_has_conflict \n" +
-
-            "  from current_version cv";
+              FROM current_version AS cv
+        """;
 
     /**
      * Получение подробностей о версиях, структуре и наличии конфликтов.
      */
-    @Query(nativeQuery = true, value = FIND_BY_VERSION_ID)
+    @Query(nativeQuery = true,
+            value = WITH_CURRENT_VERSION_BY_ID + SELECT_FROM_CURRENT_VERSION)
     RefBookDetailModel findByVersionId(@Param("currentVersionId") Integer currentVersionId);
 }
