23ce04325970ccc67395ccdc8366efd16a78962b
arahmatullin <arahmatullin@i-novus.ru>
2025-04-27 13:41:54 +0300

    cf Refactor file exception
    https://jira.i-novus.ru/browse/RDM-1172

--- rdm-api/src/main/java/ru/i_novus/ms/rdm/api/exception/FileException.java
+++ rdm-api/src/main/java/ru/i_novus/ms/rdm/api/exception/FileException.java
@@ -5,22 +5,27 @@ import net.n2oapp.platform.i18n.UserException;
 
 public class FileException extends UserException {
 
-    private static final String ABSENT_FILE_EXTENSION_EXCEPTION = "file.extension.absent";
-    private static final String INVALID_FILE_EXTENSION_EXCEPTION = "file.extension.invalid";
+    private static final String ABSENT_FILE_NAME_EXCEPTION_CODE = "file.name.absent";
+    private static final String ABSENT_FILE_EXTENSION_EXCEPTION_CODE = "file.extension.absent";
+    private static final String INVALID_FILE_EXTENSION_EXCEPTION_CODE = "file.extension.invalid";
 
-    protected FileException(Message message) {
+    public FileException(Message message) {
         super(message);
     }
 
-    protected FileException(Message message, Throwable cause) {
+    public FileException(Message message, Throwable cause) {
         super(message, cause);
     }
 
+    public static FileException newAbsentFileNameException() {
+        return new FileException(new Message(ABSENT_FILE_NAME_EXCEPTION_CODE));
+    }
+
     public static FileException newAbsentFileExtensionException(String filename) {
-        return new FileException(new Message(ABSENT_FILE_EXTENSION_EXCEPTION, filename));
+        return new FileException(new Message(ABSENT_FILE_EXTENSION_EXCEPTION_CODE, filename));
     }
 
     public static FileException newInvalidFileExtensionException(String invalidExtension) {
-        return new FileException(new Message(INVALID_FILE_EXTENSION_EXCEPTION, invalidExtension));
+        return new FileException(new Message(INVALID_FILE_EXTENSION_EXCEPTION_CODE, invalidExtension));
     }
 }
