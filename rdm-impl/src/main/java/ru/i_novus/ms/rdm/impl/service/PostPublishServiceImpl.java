31bf17750998e3e1a9db50d9b292b32fdc567479
arahmatullin <arahmatullin@i-novus.ru>
2025-07-04 12:42:57 +0300

    Merge post-publish handler to service
    https://jira.i-novus.ru/browse/RDM-1175

--- rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/service/PostPublishServiceImpl.java
+++ rdm-impl/src/main/java/ru/i_novus/ms/rdm/impl/service/PostPublishServiceImpl.java
@@ -6,14 +6,17 @@ import net.n2oapp.platform.i18n.UserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import ru.i_novus.ms.rdm.api.model.draft.PostPublishRequest;
-import ru.i_novus.ms.rdm.impl.provider.PostPublishResolverHandler;
+import ru.i_novus.ms.rdm.impl.provider.PostPublishResolver;
 import ru.i_novus.ms.rdm.impl.service.async.AsyncShedLockService;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collection;
+
+import static java.util.Collections.emptyList;
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Service
 @SuppressWarnings("unused")
@@ -24,17 +27,20 @@ public class PostPublishServiceImpl implements PostPublishService {
     private static final String POST_PUBLISH_JOB_NAME_FORMAT = "post_publish_%s";
     private static final int POST_PUBLISH_MAX_LIVE_HOURS = 1;
 
-    private final PostPublishResolverHandler resolverHandler;
+    private static final String LOG_OPERATION_HANDLING_REQUEST = "Handle operation to resolve post-publish request: {}";
+    private static final String LOG_OPERATION_RESOLVERS_NOT_FOUND = "Post-publish resolvers not found";
 
     private final AsyncShedLockService asyncShedLockService;
 
+    private final Collection<PostPublishResolver> resolvers;
+
     @Autowired
     public PostPublishServiceImpl(
-            @Lazy PostPublishResolverHandler resolverHandler,
-            AsyncShedLockService asyncShedLockService
+            AsyncShedLockService asyncShedLockService,
+            Collection<PostPublishResolver> resolvers
     ) {
-        this.resolverHandler = resolverHandler;
         this.asyncShedLockService = asyncShedLockService;
+        this.resolvers = !isEmpty(resolvers) ? resolvers : emptyList();
     }
 
     public void process(PostPublishRequest request) {
@@ -64,7 +70,22 @@ public class PostPublishServiceImpl implements PostPublishService {
 
     private void runPostPublish(PostPublishRequest request) {
 
-        resolverHandler.handle(request);
+        if (logger.isInfoEnabled()) {
+            logger.info(LOG_OPERATION_HANDLING_REQUEST, request);
+        }
+
+        if (isEmpty(resolvers)) {
+            logger.warn(LOG_OPERATION_RESOLVERS_NOT_FOUND);
+            return;
+        }
+
+        resolvers.forEach(resolver -> resolve(resolver, request));
+    }
+
+    private void resolve(PostPublishResolver resolver, PostPublishRequest request) {
+
+        // to-do: Обработка ошибок?!
+        resolver.resolve(request);
     }
 
     private UserException lockError(String code) {
